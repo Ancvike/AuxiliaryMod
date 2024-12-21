332efd8ac383b9e5df864ce5b10fc90fa4be3ddf
Ancvike <2047870696@qq.com>
2024-12-21 13:10:43 +0800

    测试

---
+++ src/FullResource/ui/uiFragments/FragmentManager.java
@@ -0,0 +1,14 @@
+package FullResource.ui.uiFragments;
+
+import FullResource.ui.SidebarSwitcher;
+import FullResource.ui.sidebarWindows.WindowManager;
+
+public class FragmentManager {
+    public static SidebarSwitcher sidebarSwitcherFragment;
+
+    public static void init() {
+        sidebarSwitcherFragment = new SidebarSwitcher(
+                WindowManager.body
+        );
+    }
+}
