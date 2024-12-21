95f4a01b71bae0a5d9d3202cd113ad8ace6e7353
Ancvike <2047870696@qq.com>
2024-12-21 13:10:43 +0800

    测试

--- src/FullResource/FullResource.java
+++ src/FullResource/FullResource.java
@@ -1,6 +1,8 @@
 package FullResource;
 
+import FullResource.core.Core;
 import FullResource.ui.coreSetting.SharSettingUI;
+import FullResource.ui.uiFragments.FragmentManager;
 import FullResource.ui.sidebarWindows.WindowManager;
 import arc.Events;
 import mindustry.game.EventType;
--- src/FullResource/ui/coreSetting/SharSettingUI.java
+++ src/FullResource/ui/coreSetting/SharSettingUI.java
@@ -4,19 +4,22 @@ import FullResource.ui.SidebarSwitcher;
 import FullResource.ui.sidebarWindows.WindowManager;
 import arc.struct.Seq;
 
+import static FullResource.ui.uiFragments.FragmentManager.sidebarSwitcherFragment;
 import static mindustry.Vars.mobile;
 
 public class SharSettingUI {
-    public static SidebarSwitcher sidebarSwitcherFragment;
 
     public static void init() {
-        Seq<SharSetting> tapSeq = new Seq<>();
-        addGraphicCheckSetting("sidebar", !mobile, tapSeq, () -> sidebarSwitcherFragment.rebuildSidebarTable());
         sidebarSwitcherFragment = new SidebarSwitcher(
                 WindowManager.body
         );
+        Seq<SharSetting> tapSeq = new Seq<>();
+        addGraphicCheckSetting("sidebar", !mobile, tapSeq, () -> sidebarSwitcherFragment.rebuildSidebarTable());
+
     }
+
     public static void addGraphicCheckSetting(String key, boolean def, Seq<SharSetting> list, Runnable onSetted) {
-        list.add(new SharSetting(key, def) {});
+        list.add(new SharSetting(key, def) {
+        });
     }
 }
