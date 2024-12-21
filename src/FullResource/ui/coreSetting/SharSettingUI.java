e4f27fddf28500cc2dcf5f8e0b0fdd78dd63fe58
Ancvike <2047870696@qq.com>
2024-12-21 13:12:47 +0800

    测试

--- src/FullResource/ui/coreSetting/SharSettingUI.java
+++ src/FullResource/ui/coreSetting/SharSettingUI.java
@@ -1,7 +1,5 @@
 package FullResource.ui.coreSetting;
 
-import FullResource.ui.SidebarSwitcher;
-import FullResource.ui.sidebarWindows.WindowManager;
 import arc.struct.Seq;
 
 import static FullResource.ui.FragmentManager.sidebarSwitcherFragment;
@@ -10,9 +8,6 @@ import static mindustry.Vars.mobile;
 public class SharSettingUI {
 
     public static void init() {
-        sidebarSwitcherFragment = new SidebarSwitcher(
-                WindowManager.body
-        );
         Seq<SharSetting> tapSeq = new Seq<>();
         addGraphicCheckSetting("sidebar", !mobile, tapSeq, () -> sidebarSwitcherFragment.rebuildSidebarTable());
 
