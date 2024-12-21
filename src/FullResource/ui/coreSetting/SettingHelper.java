6ae78e4cbffb3329fe24b86db742aa2a1f9cfffd
Ancvike <2047870696@qq.com>
2024-12-21 10:46:47 +0800

    测试

--- src/FullResource/ui/coreSetting/SettingHelper.java
+++
@@ -1,9 +0,0 @@
-package FullResource.ui.coreSetting;
-
-import arc.struct.Seq;
-
-public class SettingHelper {
-    public static void addGraphicCheckSetting(String key, boolean def, Seq<SharSetting> list, Runnable onSetted) {
-        list.add(new SharSetting(key, def) {});
-    }
-}
