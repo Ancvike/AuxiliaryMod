18c31912ed1cb975c8ea9a353dcd3f6fe8df625d
Ancvike <2047870696@qq.com>
2024-12-21 12:09:32 +0800

    测试

--- src/FullResource/ui/uiFragments/FragmentManager.java
+++ src/FullResource/ui/uiFragments/FragmentManager.java
@@ -2,17 +2,13 @@ package FullResource.ui.uiFragments;
 
 import FullResource.ui.SidebarSwitcher;
 import FullResource.ui.sidebarWindows.WindowManager;
-import mindustry.ui.dialogs.BaseDialog;
 
 public class FragmentManager {
     public static SidebarSwitcher sidebarSwitcherFragment;
 
     public static void init() {
-//        sidebarSwitcherFragment = new SidebarSwitcher(
+        sidebarSwitcherFragment = new SidebarSwitcher(
 //                WindowManager.body
-//        );
-        BaseDialog baseDialog = new BaseDialog("按钮测试页");
-        baseDialog.addCloseButton();
-        baseDialog.show();
+        );
     }
 }
