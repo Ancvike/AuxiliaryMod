22c82e87b5435db927cb49f2ab488f8bccb08735
Ancvike <2047870696@qq.com>
2024-12-21 13:06:07 +0800

    测试

--- src/FullResource/ui/coreSetting/SharSettingUI.java
+++ src/FullResource/ui/coreSetting/SharSettingUI.java
@@ -10,12 +10,11 @@ public class SharSettingUI {
     public static SidebarSwitcher sidebarSwitcherFragment;
 
     public static void init() {
+        Seq<SharSetting> tapSeq = new Seq<>();
+        addGraphicCheckSetting("sidebar", !mobile, tapSeq, () -> sidebarSwitcherFragment.rebuildSidebarTable());
         sidebarSwitcherFragment = new SidebarSwitcher(
                 WindowManager.body
         );
-        Seq<SharSetting> tapSeq = new Seq<>();
-        addGraphicCheckSetting("sidebar", !mobile, tapSeq, () -> sidebarSwitcherFragment.rebuildSidebarTable());
-
     }
     public static void addGraphicCheckSetting(String key, boolean def, Seq<SharSetting> list, Runnable onSetted) {
         list.add(new SharSetting(key, def) {});
