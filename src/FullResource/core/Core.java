dc256c4d4ca751690720773f53aa0eca3f326065
Ancvike <2047870696@qq.com>
2024-12-24 18:49:07 +0800

    测试

--- src/FullResource/core/Core.java
+++ src/FullResource/core/Core.java
@@ -1,12 +1,36 @@
 package FullResource.core;
 
+import arc.scene.ui.layout.Table;
 import mindustry.Vars;
+import mindustry.ui.Styles;
+
+import static FullResource.ui.WindowManager.coreWindow;
 
 public class Core {
+    public static Table body;
 
     public Core() {
-        Vars.ui.hudGroup.fill(t -> {
+        body = new Table(t -> {
+            t.name = "Window Buttons";
+            t.left();
 
+            t.button(coreWindow.icon, Styles.emptyi, () -> {
+                coreWindow.parent.setLayoutEnabled(false);
+                coreWindow.toggle();
+
+                coreWindow.setLayoutEnabled(true);
+
+            }).size(40f).tooltip(tt -> {
+                tt.setBackground(Styles.black6);
+                tt.label(() -> "66666").pad(2f);
+            });
+            t.row();
+        });
+
+        Vars.ui.hudGroup.fill(t -> {
+            t.add(body);
+            t.top();
+            t.x = 300;
         });
     }
 }
--- src/FullResource/ui/WindowManager.java
+++ src/FullResource/ui/WindowManager.java
@@ -1,14 +1,12 @@
 package FullResource.ui;
 
 import arc.scene.ui.layout.Table;
-import arc.struct.*;
 import mindustry.*;
 import mindustry.ui.*;
 
 
 public class WindowManager {
     public static CoreWindow coreWindow = new CoreWindow();
-    public static Table body;
 
     public static void init() {
 
@@ -18,29 +16,6 @@ public class WindowManager {
 
             coreWindow.build();
             t.add(coreWindow).height(coreWindow.getHeight()).width(coreWindow.getWidth());
-        });
-
-        body = new Table(t -> {
-            t.name = "Window Buttons";
-            t.left();
-
-            t.button(coreWindow.icon, Styles.emptyi, () -> {
-                coreWindow.parent.setLayoutEnabled(false);
-                coreWindow.toggle();
-
-                coreWindow.setLayoutEnabled(true);
-
-            }).size(40f).tooltip(tt -> {
-                tt.setBackground(Styles.black6);
-                tt.label(() -> "66666").pad(2f);
-            });
-            t.row();
-        });
-
-        Vars.ui.hudGroup.fill(t -> {
-            t.add(body);
-            t.top();
-            t.x = 300;
         });
     }
 }
