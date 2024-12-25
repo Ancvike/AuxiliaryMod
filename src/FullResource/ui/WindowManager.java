aa0105bfc10d0ccbb4e723d3044a5b65a45ec8a3
Ancvike <2047870696@qq.com>
2024-12-25 22:25:41 +0800

    测试

--- src/FullResource/ui/WindowManager.java
+++ src/FullResource/ui/WindowManager.java
@@ -1,12 +1,13 @@
 package FullResource.ui;
 
 import mindustry.Vars;
+import mindustry.ui.dialogs.BaseDialog;
 
 public class WindowManager {
     public static CoreWindow coreWindow = new CoreWindow();
+    public static final BaseDialog dialog_no = new BaseDialog("失败");
 
     public static void init() {
-
         // windows place for dragging
         Vars.ui.hudGroup.fill(t -> {
             t.name = "Windows";
@@ -14,5 +15,12 @@ public class WindowManager {
             coreWindow.build();
             t.add(coreWindow).height(coreWindow.getHeight()).width(coreWindow.getWidth());
         });
+
+        setDialog_no();
+    }
+
+    public static void setDialog_no() {
+        dialog_no.cont.add("区块未占领,无法使用该功能").row();
+        dialog_no.cont.button("确认", dialog_no::hide).size(100f, 50f);
     }
 }
