3f554c79711bcdaf581516df6f689305191cf9d7
Ancvike <2047870696@qq.com>
2024-12-25 22:00:57 +0800

    测试

--- src/FullResource/core/Core.java
+++ src/FullResource/core/Core.java
@@ -10,30 +10,16 @@ import static mindustry.Vars.state;
 import static FullResource.ui.WindowManager.coreWindow;
 
 public class Core {
-    Table body;
     public static final BaseDialog dialog_no = new BaseDialog("失败");
 
     public Core() {
         setDialog_no();
-        body = new Table(t -> {
-            t.name = "Window Buttons";
-            t.left();
-
-            t.button(Icon.upload, Styles.emptyi, () -> {
-                coreWindow.parent.setLayoutEnabled(false);
-                coreWindow.toggle();
-
-                coreWindow.setLayoutEnabled(true);
-
-            }).size(40f).tooltip(tt -> {
-                tt.setBackground(Styles.black6);
-                tt.label(() -> "66666").pad(2f);
-            });
-            t.row();
-        });
 
         Vars.ui.hudGroup.fill(t -> {
-            t.add(body);
+            t.button(Icon.upload, Styles.emptyi, this::onClick).size(40f).tooltip(tt -> {
+                tt.setBackground(Styles.black6);
+                tt.label(() -> "full").pad(2f);
+            });
             t.top();
             t.x = 300;
         });
@@ -42,9 +28,16 @@ public class Core {
     public void onClick() {
         if (!state.rules.waves && state.isCampaign()) {//区块是否占领
         //先检测核心是哪个,并get资源量及上限
+            success();
         } else {
             dialog_no.show();
         }
+    }
+
+    public void success() {
+        coreWindow.parent.setLayoutEnabled(false);
+        coreWindow.toggle();
+        coreWindow.setLayoutEnabled(true);
     }
 
     public static void setDialog_no() {
