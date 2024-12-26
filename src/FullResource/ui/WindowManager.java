b48be265735696352171f931043e5337608c2b15
Ancvike <2047870696@qq.com>
2024-12-26 11:10:08 +0800

    测试

--- src/FullResource/ui/WindowManager.java
+++ src/FullResource/ui/WindowManager.java
@@ -1,11 +1,13 @@
 package FullResource.ui;
 
+import arc.scene.ui.layout.Table;
 import mindustry.Vars;
 import mindustry.ui.dialogs.BaseDialog;
 
 public class WindowManager {
     public static CoreWindow coreWindow = new CoreWindow();
     public static final BaseDialog dialog_no = new BaseDialog("失败");
+    public static final BaseDialog dialog_yes = new BaseDialog("");
 
     public static void init() {
         // windows place for dragging
@@ -17,10 +19,21 @@ public class WindowManager {
         });
 
         setDialog_no();
+        setDialog_yes();
     }
 
     public static void setDialog_no() {
         dialog_no.cont.add("区块未占领,无法使用该功能").row();
-        dialog_no.cont.button("确认", dialog_no::hide).size(100f, 50f);
+        dialog_no.addCloseButton();
+    }
+
+    public static void setDialog_yes() {
+        dialog_no.cont.add("确定要这样做吗?").row();
+
+        dialog_no.button("确定", WindowManager::clearDialog_yes);
+    }
+
+    public static void clearDialog_yes() {
+        dialog_yes.clear();
     }
 }
