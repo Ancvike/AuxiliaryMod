7f7cbb45edac4f54b762713b63a40d82f78baf7e
Ancvike <2047870696@qq.com>
2024-12-26 20:34:48 +0800

    测试

--- src/FullResource/ui/Dialogs.java
+++ src/FullResource/ui/Dialogs.java
@@ -15,7 +15,7 @@ import static mindustry.game.Team.sharded;
 public class Dialogs {
     public static final BaseDialog dialog_no = new BaseDialog("失败");
     public static final BaseDialog dialog_yes = new BaseDialog("");
-    private static Table itemTable;
+    private static Table itemTable = new Table();
 
     public static void init() {
         setDialog_no();
