a61fb116f0ccd270994f130f6c15c21db6f96734
Ancvike <2047870696@qq.com>
2024-12-28 12:36:44 +0800

    测试

--- src/FullResource/core/Core.java
+++ src/FullResource/core/Core.java
@@ -1,12 +1,19 @@
 package FullResource.core;
 
-import FullResource.ui.Dialogs;
 import arc.scene.ui.layout.Table;
+import mindustry.Vars;
+import mindustry.core.UI;
+import mindustry.type.Item;
+import mindustry.ui.Styles;
+import mindustry.world.blocks.storage.CoreBlock;
 
 import static FullResource.ui.Dialogs.*;
+import static mindustry.Vars.iconSmall;
 import static mindustry.Vars.state;
+import static mindustry.game.Team.sharded;
 
 public class Core {
+    private static Table itemTable;
 
     public static void init() {
     }
@@ -14,16 +21,41 @@ public class Core {
     public static void onClick() {
         if (!state.rules.waves && state.isCampaign()) {//区块是否占领
             //先检测核心是哪个,并get资源量及上限
+            itemTable = setTable();
+            setDialog_yes(itemTable);
             dialog_yes.show();
         } else {
             dialog_no.show();
         }
     }
 
-    public static void click_yes() {
+    public static Table setTable() {
+        return new Table(t -> {
+            t.add("资源列表").row();
+            CoreBlock.CoreBuild core = sharded.core();
+            if (core == null || core.items == null) {
+                return;
+            }
+            for (int i = 0; i < Vars.content.items().size; i++) {
+                Item item = Vars.content.item(i);
+                if (!sharded.items().has(item)) continue;
+                t.image(item.uiIcon).size(iconSmall).tooltip(tt -> tt.background(Styles.black6).add(item.localizedName).style(Styles.outlineLabel).margin(2f));
+                t.add(UI.formatAmount(core.items.get(item))).minWidth(5 * 8f).left();
+            }
+            t.button("确认", Core::click_yes);
+            t.button("取消", Core::click_no);
+        });
+    }
+
+    public static void click_no() {
+        resetDialog(itemTable);
         dialog_yes.hide();
     }
-    public static void click_no() {
-        dialog_yes.hide();
+    public static void click_yes() {
+        changeItems();
+        click_no();
+    }
+
+    public static void changeItems() {
     }
 }
--- src/FullResource/ui/Dialogs.java
+++ src/FullResource/ui/Dialogs.java
@@ -1,16 +1,7 @@
 package FullResource.ui;
 
-
 import arc.scene.ui.layout.Table;
-import mindustry.Vars;
-import mindustry.core.UI;
-import mindustry.type.Item;
-import mindustry.ui.Styles;
 import mindustry.ui.dialogs.BaseDialog;
-import mindustry.world.blocks.storage.CoreBlock;
-
-import static mindustry.Vars.iconSmall;
-import static mindustry.game.Team.sharded;
 
 public class Dialogs {
     public static final BaseDialog dialog_no = new BaseDialog("失败");
@@ -25,28 +16,11 @@ public class Dialogs {
         dialog_no.addCloseButton();
     }
 
-    public static void setDialog_yes() {
-        dialog_yes.add();
+    public static void setDialog_yes(Table itemTable) {
+        dialog_yes.add(itemTable);
     }
 
-    public static Table setTable() {
-        return new Table(t -> {
-            t.add("资源列表").row();
-            CoreBlock.CoreBuild core = sharded.core();
-            if (core == null || core.items == null) {
-                return;
-            }
-            for (int i = 0; i < Vars.content.items().size; i++) {
-                Item item = Vars.content.item(i);
-                if (!sharded.items().has(item)) continue;
-                t.image(item.uiIcon).size(iconSmall).tooltip(tt -> tt.background(Styles.black6).add(item.localizedName).style(Styles.outlineLabel).margin(2f));
-                t.add(UI.formatAmount(core.items.get(item))).minWidth(5 * 8f).left();
-            }
-            t.button("", () -> {});
-            t.button("", () -> {});
-        });
-    }
-
-    public static void resetDialog() {
+    public static void resetDialog(Table itemTable) {
+        dialog_yes.removeChild(itemTable);
     }
 }
