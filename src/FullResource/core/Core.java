ad611ad2aa4d87743a032776b2dd9c77825d807b
Ancvike <2047870696@qq.com>
2024-12-28 11:09:17 +0800

    测试

--- src/FullResource/core/Core.java
+++ src/FullResource/core/Core.java
@@ -1,11 +1,13 @@
 package FullResource.core;
 
 import FullResource.ui.Dialogs;
+import arc.scene.ui.layout.Table;
 
 import static FullResource.ui.Dialogs.*;
 import static mindustry.Vars.state;
 
 public class Core {
+    public static Table itemTable = new Table();
 
     public static void init() {
     }
@@ -13,8 +15,8 @@ public class Core {
     public static void onClick() {
         if (!state.rules.waves && state.isCampaign()) {//区块是否占领
             //先检测核心是哪个,并get资源量及上限
-            Dialogs.getCoreItems();
-            Dialogs.setDialog_yes();
+            itemTable = Dialogs.getCoreItems();
+            Dialogs.setDialog_yes(itemTable);
             dialog_yes.show();
         } else {
             dialog_no.show();
@@ -22,11 +24,11 @@ public class Core {
     }
 
     public static void click_yes() {
-        resetDialog();
+        resetDialog(itemTable);
         dialog_yes.hide();
     }
     public static void click_no() {
-        resetDialog();
+        resetDialog(itemTable);
         dialog_yes.hide();
     }
 }
