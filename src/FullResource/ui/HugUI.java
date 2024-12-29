eb478ad3509b1e5800be470dd6748cf0d6f2b081
Ancvike <2047870696@qq.com>
2024-12-29 15:42:10 +0800

    测试

--- src/FullResource/ui/HugUI.java
+++ src/FullResource/ui/HugUI.java
@@ -8,7 +8,7 @@ import mindustry.ui.Styles;
 public class HugUI {
     public static void init() {
         Vars.ui.hudGroup.fill(t -> {
-            t.button(Icon.info, Styles.emptyi, Core::onClick).size(40f).tooltip(tt -> {
+            t.button(Icon.info, Core::onClick).size(40f).tooltip(tt -> {
                 tt.setBackground(Styles.black6);
                 tt.label(() -> "full").pad(2f);
             });
