a5107c32b5b51555b378e8df97c32fcc91b30042
Ancvike <2047870696@qq.com>
2024-12-29 13:32:57 +0800

    测试

--- src/FullResource/core/Core.java
+++ src/FullResource/core/Core.java
@@ -33,12 +33,15 @@ public class Core {
             t.add("资源列表").row();
             CoreBlock.CoreBuild core = sharded.core();
             if (core == null || core.items == null) {
+                t.add("核心内无资源");
                 return;
             }
             for (int i = 0; i < Vars.content.items().size; i++) {
                 Item item = Vars.content.item(i);
                 if (!sharded.items().has(item)) continue;
-                t.image(item.uiIcon).size(iconSmall).tooltip(tt -> tt.background(Styles.black6).add(item.localizedName).style(Styles.outlineLabel));
+                t.image(item.uiIcon).size(iconSmall)
+//                      .tooltip(tt -> tt.background(Styles.black6).add(item.localizedName).style(Styles.outlineLabel))
+                ;
                 t.add(UI.formatAmount(core.items.get(item)));
                 t.add("->");
                 t.image(item.uiIcon).size(iconSmall).tooltip(tt -> tt.background(Styles.black6).add(item.localizedName).style(Styles.outlineLabel));
