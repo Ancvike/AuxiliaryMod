0bf4428847c7be90df859fdf473c541ac5bb64b3
Ancvike <2047870696@qq.com>
2024-12-30 20:27:58 +0800

    测试

--- src/auxiliary/core/BuildingRestoration.java
+++ src/auxiliary/core/BuildingRestoration.java
@@ -12,7 +12,7 @@ public class BuildingRestoration {
 
     public static void init() {
         table.add("你确定要修复所有建筑吗?").size(200f, 50f);
-        table.button("确认", BuildingRestoration::click_yes).size(120f, 50f);
+        table.button("确定", BuildingRestoration::click_yes).size(120f, 50f);
         table.button("取消", BuildingRestoration::click_no).size(120f, 50f);
         Dialogs.setDialog_yes(dialog_restoration, table);
     }
