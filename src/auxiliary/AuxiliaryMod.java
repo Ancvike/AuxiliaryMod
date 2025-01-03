d1e71003584cc9dbadddc10c1ced27a80611683d
Ancvike <2047870696@qq.com>
2025-01-03 20:51:22 +0800

    测试

--- src/auxiliary/AuxiliaryMod.java
+++ src/auxiliary/AuxiliaryMod.java
@@ -4,6 +4,7 @@ import auxiliary.core.BuildingRestoration;
 import auxiliary.ui.HugUI;
 import auxiliary.ui.Dialogs;
 import arc.Events;
+import auxiliary.ui.SettingUI;
 import mindustry.game.EventType;
 import mindustry.mod.Mod;
 
@@ -11,6 +12,7 @@ public class AuxiliaryMod extends Mod {
     public void init() {
         Events.on(EventType.ClientLoadEvent.class, e -> {
             HugUI.init();
+            SettingUI.init();
             Dialogs.init();
             BuildingRestoration.init();
         });
