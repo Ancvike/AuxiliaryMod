695e486fd71105a34ceaf6c9a995d042a536a654
Ancvike <2047870696@qq.com>
2025-01-04 22:53:21 +0800

    测试

--- src/auxiliary/ui/SettingUI.java
+++ src/auxiliary/ui/SettingUI.java
@@ -7,7 +7,7 @@ import static auxiliary.dialogs.Dialogs.dialog_setting;
 public class SettingUI {
 
     public static void init() {
-        if (Core.app.isDesktop()) {
+        if (!Core.app.isDesktop()) {
             dialog_setting.show();
         }
     }
