a7c3b2045a9b50ef6978c2e64651b36c401cddfb
Ancvike <2047870696@qq.com>
2025-01-04 14:41:06 +0800

    测试

--- src/auxiliary/core/binding/BindingManager.java
+++ src/auxiliary/core/binding/BindingManager.java
@@ -1,16 +1,16 @@
-package auxiliary.core.binding;
-
-import mindustry.ui.dialogs.BaseDialog;
-
-import static arc.Core.input;
-import static auxiliary.core.binding.Binding.UP;
-
-public class BindingManager {
-    public static void init() {
-        if (input.keyTap(UP)) {
-            BaseDialog dialog = new BaseDialog("test");
-            dialog.addCloseButton();
-            dialog.show();
-        }
-    }
-}
+//package auxiliary.core.binding;
+//
+//import mindustry.ui.dialogs.BaseDialog;
+//
+//import static arc.Core.input;
+//import static auxiliary.core.binding.Binding.UP;
+//
+//public class BindingManager {
+//    public static void init() {
+//        if (input.keyTap(UP)) {
+//            BaseDialog dialog = new BaseDialog("test");
+//            dialog.addCloseButton();
+//            dialog.show();
+//        }
+//    }
+//}
