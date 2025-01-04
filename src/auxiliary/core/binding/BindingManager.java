68a34ee4cde66f8f3b2cf193fb622ba648c10d87
Ancvike <2047870696@qq.com>
2025-01-04 15:42:43 +0800

    测试

--- src/auxiliary/core/binding/BindingManager.java
+++ src/auxiliary/core/binding/BindingManager.java
@@ -1,6 +1,7 @@
 package auxiliary.core.binding;
 
 import arc.input.KeyCode;
+import mindustry.Vars;
 import mindustry.ui.dialogs.BaseDialog;
 
 import static arc.Core.input;
@@ -8,9 +9,9 @@ import static arc.Core.input;
 public class BindingManager {
     public static void init() {
         if (input.keyTap(KeyCode.u)) {
-            BaseDialog dialog = new BaseDialog("Test");
-            dialog.addCloseButton();
-            dialog.show();
+            Vars.ui.hudGroup.fill(t -> {
+                t.button("1111111111111", () -> {}).left().bottom();
+            });
         }
     }
 }
