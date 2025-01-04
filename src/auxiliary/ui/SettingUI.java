05b2ff41c78a5d9453aa95e1ae49ed392754d303
Ancvike <2047870696@qq.com>
2025-01-04 13:00:03 +0800

    测试

--- src/auxiliary/ui/SettingUI.java
+++ src/auxiliary/ui/SettingUI.java
@@ -1,11 +1,11 @@
 package auxiliary.ui;
 
-import arc.Input;
 import arc.input.KeyCode;
 import mindustry.Vars;
 
+import static arc.Core.input;
+
 public class SettingUI {
-    public static Input input;
     public static void init() {
         Vars.ui.settings.addCategory("AuxiliaryMod设置", table -> input.keyTap(KeyCode.u));
     }
