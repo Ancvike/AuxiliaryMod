f19912de5f3c43dc9c7123015081ced70206fdd8
Ancvike <2047870696@qq.com>
2025-01-04 22:20:23 +0800

    测试

--- src/auxiliary/core/Function/FunctionManager.java
+++ src/auxiliary/core/Function/FunctionManager.java
@@ -2,6 +2,7 @@ package auxiliary.core.Function;
 
 public class FunctionManager {
     public static void init() {
+        new Setting().init();
         FullResource.init();
         BuildingRestoration.init();
         ConveyorUP.init();
--- src/auxiliary/ui/SettingUI.java
+++ src/auxiliary/ui/SettingUI.java
@@ -1,8 +1,18 @@
 package auxiliary.ui;
 
-public class SettingUI{
+import arc.Core;
+import mindustry.Vars;
+
+import static auxiliary.core.Function.Setting.settingTable;
+import static auxiliary.ui.Dialogs.dialog_setting;
+
+public class SettingUI {
 
     public void init() {
-
+        if (!Core.app.isDesktop()) {
+            dialog_setting.show();
+        } else {
+            Vars.ui.settings.addCategory("AuxiliaryMod设置", settingTable);
+        }
     }
 }
