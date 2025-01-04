acec7b41a07dea55fb9f19912151998ed49512c3
Ancvike <2047870696@qq.com>
2025-01-04 15:04:45 +0800

    测试

--- src/auxiliary/core/binding/BindingManager.java
+++ src/auxiliary/core/binding/BindingManager.java
@@ -7,14 +7,10 @@ import static auxiliary.core.binding.Binding.UP;
 
 public class BindingManager {
     public static void init() {
-        try {
-            if (input.keyTap(UP)) {
-                BaseDialog dialog = new BaseDialog("test");
-                dialog.addCloseButton();
-                dialog.show();
-            }
-        } catch (Exception e) {
-            System.out.println("errorrrrrrrrrrrrrrrrrrrrrrrr");
+        if (input.keyTap(UP)) {
+            BaseDialog dialog = new BaseDialog("test");
+            dialog.addCloseButton();
+            dialog.show();
         }
     }
 }
