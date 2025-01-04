0ea4bbdfc400715cf80333cb091949972331e7d9
Ancvike <2047870696@qq.com>
2025-01-04 14:41:06 +0800

    测试

--- src/auxiliary/core/binding/Binding.java
+++ src/auxiliary/core/binding/Binding.java
@@ -1,31 +1,31 @@
-package auxiliary.core.binding;
-
-import arc.KeyBinds;
-import arc.input.InputDevice;
-import arc.input.KeyCode;
-
-public enum Binding implements KeyBinds.KeyBind {
-    UP(KeyCode.u);
-
-    private final KeyBinds.KeybindValue defaultValue;
-    private final String category;
-
-    Binding(KeyBinds.KeybindValue defaultValue, String category) {
-        this.defaultValue = defaultValue;
-        this.category = category;
-    }
-
-    Binding(KeyBinds.KeybindValue defaultValue) {
-        this(defaultValue, null);
-    }
-
-    @Override
-    public KeyBinds.KeybindValue defaultValue(InputDevice.DeviceType type) {
-        return defaultValue;
-    }
-
-    @Override
-    public String category() {
-        return category;
-    }
-}
+//package auxiliary.core.binding;
+//
+//import arc.KeyBinds;
+//import arc.input.InputDevice;
+//import arc.input.KeyCode;
+//
+//public enum Binding implements KeyBinds.KeyBind {
+//    UP(KeyCode.u);
+//
+//    private final KeyBinds.KeybindValue defaultValue;
+//    private final String category;
+//
+//    Binding(KeyBinds.KeybindValue defaultValue, String category) {
+//        this.defaultValue = defaultValue;
+//        this.category = category;
+//    }
+//
+//    Binding(KeyBinds.KeybindValue defaultValue) {
+//        this(defaultValue, null);
+//    }
+//
+//    @Override
+//    public KeyBinds.KeybindValue defaultValue(InputDevice.DeviceType type) {
+//        return defaultValue;
+//    }
+//
+//    @Override
+//    public String category() {
+//        return category;
+//    }
+//}
