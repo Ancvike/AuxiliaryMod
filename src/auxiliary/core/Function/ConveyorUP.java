62dc0bad0ce24e73c6fae565bd388c9c0a9c64d0
Ancvike <2047870696@qq.com>
2025-01-05 16:15:46 +0800

    测试

--- src/auxiliary/core/Function/ConveyorUP.java
+++ src/auxiliary/core/Function/ConveyorUP.java
@@ -1,29 +1,8 @@
 package auxiliary.core.Function;
 
-import arc.input.KeyCode;
-import arc.scene.event.InputEvent;
-import arc.scene.event.InputListener;
-import mindustry.Vars;
-import mindustry.ui.dialogs.BaseDialog;
-
 public class ConveyorUP {
 
     public static void init() {
-        Vars.ui.hudGroup.fill(t -> {
-            t.addListener(new InputListener() {
-                @Override
-                public boolean keyDown(InputEvent event, KeyCode keycode) {
-                    if (keycode == KeyCode.u) {
-                        load();
-                    }
-                    return false;
-                }
-            });
-        });
-    }
-    public static void load () {
-        BaseDialog dialog = new BaseDialog("");
-        dialog.addCloseButton();
-        dialog.show();
+
     }
 }
