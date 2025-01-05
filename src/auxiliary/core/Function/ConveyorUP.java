53cd42e2d7c294bcd07af7ab2c944c77f61fb54c
Ancvike <2047870696@qq.com>
2025-01-05 15:28:20 +0800

    测试

--- src/auxiliary/core/Function/ConveyorUP.java
+++ src/auxiliary/core/Function/ConveyorUP.java
@@ -1,27 +1,8 @@
 package auxiliary.core.Function;
 
-import arc.Core;
-import mindustry.Vars;
-import mindustry.input.Binding;
-import mindustry.ui.dialogs.BaseDialog;
-
-import static arc.Core.assets;
-
 public class ConveyorUP {
 
     public static void init() {
-        //????????????????????????????????
-        if (assets.update(500)) {
-            if (!Core.app.isDesktop()) return;
-            if (Vars.state.isGame()) {
-                if (!Core.scene.hasKeyboard()) {
-                    if (Core.input.keyDown(Binding.schematic_select)) {
-                        BaseDialog dialog = new BaseDialog("");
-                        dialog.addCloseButton();
-                        dialog.show();
-                    }
-                }
-            }
-        }
+
     }
 }
