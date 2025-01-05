dae043186d26535de162137a283fcae853b2c78e
Ancvike <2047870696@qq.com>
2025-01-05 10:34:48 +0800

    测试

--- src/auxiliary/core/binding/MyKeyBind.java
+++ src/auxiliary/core/binding/MyKeyBind.java
@@ -2,10 +2,25 @@ package auxiliary.core.binding;
 
 import arc.input.KeyCode;
 
-public class MyKeyBind {
-    public static KeyCode keyCode = KeyCode.u;
+public enum MyKeyBind {
+    UP(KeyCode.u, KeyCode.u);
+    private final KeyCode defaultKeyCode;
+    public KeyCode nowKeyCode;
+
+    MyKeyBind(KeyCode defaultValue, KeyCode nowKeyCode){
+        this.defaultKeyCode = defaultValue;
+        this.nowKeyCode = nowKeyCode;
+    }
 
     public static void init() {
 
+    }
+
+    public static void update(MyKeyBind key, KeyCode keyCode) {
+        key.nowKeyCode = keyCode;
+    }
+
+    public static void resetKeyBind(MyKeyBind key) {
+        key.nowKeyCode = key.defaultKeyCode;
     }
 }
--- src/auxiliary/ui/SettingUI.java
+++ src/auxiliary/ui/SettingUI.java
@@ -2,15 +2,28 @@ package auxiliary.ui;
 
 import arc.Core;
 import arc.func.Cons;
+import arc.input.KeyCode;
+import arc.scene.event.InputEvent;
+import arc.scene.event.InputListener;
+import arc.scene.ui.Dialog;
+import auxiliary.core.binding.MyKeyBind;
 import mindustry.Vars;
+import mindustry.ui.Styles;
 import mindustry.ui.dialogs.SettingsMenuDialog;
 
+import static auxiliary.core.binding.MyKeyBind.*;
 import static auxiliary.dialogs.Dialogs.dialog_setting;
 
 public class SettingUI {
     static Cons<SettingsMenuDialog.SettingsTable> settingTable = table -> {
         if (Core.app.isDesktop()) {
+            table.add("传送带升级").left().padRight(40).padLeft(8);
+            table.label(() -> UP.nowKeyCode.value).left().minWidth(90).padRight(20);
 
+            table.button("重新绑定", Styles.defaultt, () -> openDialog(UP)).width(130f);
+
+            table.button("恢复默认", Styles.defaultt, () -> resetKeyBind(UP)).width(130f).pad(2f).padLeft(4f);
+            table.row();
         } else {
             dialog_setting.show();
         }
@@ -18,5 +31,18 @@ public class SettingUI {
 
     public static void init() {
         Vars.ui.settings.addCategory("AuxiliaryMod设置", settingTable);
+    }
+
+    private static void openDialog(MyKeyBind key) {
+        Dialog rebindDialog = new Dialog("请按一个键…");
+        rebindDialog.titleTable.getCells().first().pad(4);
+        rebindDialog.addListener(new InputListener() {
+            @Override
+            public boolean keyDown(InputEvent event, KeyCode keycode) {
+                rebindDialog.hide();
+                update(key, keycode);
+                return false;
+            }
+        });
     }
 }
