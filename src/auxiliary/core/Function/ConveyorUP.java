5bcafd4b4eb11fc348e312f4adeddbb7f78c9326
Ancvike <2047870696@qq.com>
2025-01-05 11:03:34 +0800

    测试

--- src/auxiliary/core/Function/ConveyorUP.java
+++ src/auxiliary/core/Function/ConveyorUP.java
@@ -1,7 +1,18 @@
 package auxiliary.core.Function;
 
+import mindustry.ui.dialogs.BaseDialog;
+
+import static arc.Core.input;
+import static auxiliary.core.binding.MyKeyBind.UP;
+
 public class ConveyorUP {
+
     public static void init() {
 
+        if (input.keyTap(UP.nowKeyCode)) {
+            BaseDialog dialog = new BaseDialog("Conveyor UP");
+            dialog.addCloseButton();
+            dialog.show();
+        }
     }
 }
