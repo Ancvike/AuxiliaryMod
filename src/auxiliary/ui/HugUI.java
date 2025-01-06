2516432d9655832171ec0232d979f773d1e50909
Ancvike <2047870696@qq.com>
2025-01-06 14:08:50 +0800

    测试

--- src/auxiliary/ui/HugUI.java
+++ src/auxiliary/ui/HugUI.java
@@ -1,5 +1,6 @@
 package auxiliary.ui;
 
+import auxiliary.function.UIMovement;
 import mindustry.Vars;
 
 import static auxiliary.function.UIMovement.table_Building;
@@ -7,6 +8,7 @@ import static auxiliary.function.UIMovement.table_full;
 
 public class HugUI {
     public static void init() {
+        UIMovement.init();
         Vars.ui.hudGroup.addChild(table_full);
 
         Vars.ui.hudGroup.addChild(table_Building);
