7c990afd8d2023f138c17d92a8fb689f8404aa07
Ancvike <2047870696@qq.com>
2025-01-06 14:03:13 +0800

    测试

--- src/auxiliary/ui/HugUI.java
+++ src/auxiliary/ui/HugUI.java
@@ -1,25 +1,14 @@
 package auxiliary.ui;
 
-import arc.scene.ui.layout.Table;
-import auxiliary.function.BuildingRestoration;
-import auxiliary.function.FullResource;
 import mindustry.Vars;
-import mindustry.gen.Icon;
-import mindustry.ui.Styles;
 
+import static auxiliary.function.UIMovement.table_Building;
 import static auxiliary.function.UIMovement.table_full;
 
 public class HugUI {
     public static void init() {
         Vars.ui.hudGroup.addChild(table_full);
 
-        Vars.ui.hudGroup.fill(t -> {
-            t.top();
-            t.x = 450;
-            t.button(Icon.refresh1, BuildingRestoration::onClick).size(40f).tooltip(tt -> {
-                tt.setBackground(Styles.black6);
-                tt.label(() -> "建筑修复").pad(2f);
-            });
-        });
+        Vars.ui.hudGroup.addChild(table_Building);
     }
 }
