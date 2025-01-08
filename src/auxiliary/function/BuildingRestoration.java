fec3b553eff605d53efb3feb8d82308e4e10fcc7
Ancvike <2047870696@qq.com>
2025-01-08 12:51:44 +0800

    测试

--- src/auxiliary/function/BuildingRestoration.java
+++ src/auxiliary/function/BuildingRestoration.java
@@ -12,19 +12,19 @@ import static mindustry.Vars.state;
 
 public class BuildingRestoration extends Function {
     private static final BaseDialog dialog_restoration = new BaseDialog("确认页面");
-    public static final BuildingRestoration br = new BuildingRestoration();
 
     public BuildingRestoration() {
-        super("building-restoration", Icon.refresh1, br::onClick, "建筑修复");
+        super("building-restoration", Icon.refresh1, "建筑修复");
 
         Table table = new Table();
         table.add("你确定要修复所有建筑吗?").size(200f, 50f).row();
-        table.button("确定", br::click_yes).size(120f, 50f);
-        table.button("取消", br::click_no).size(120f, 50f);
+        table.button("确定", this::click_yes).size(120f, 50f);
+        table.button("取消", this::click_no).size(120f, 50f);
 
         Dialogs.setDialog_yes(dialog_restoration, table);
     }
 
+    @Override
     public void onClick() {
         if (!state.rules.waves && state.isCampaign()) {
             dialog_restoration.show();
--- src/auxiliary/function/Function.java
+++ src/auxiliary/function/Function.java
@@ -4,27 +4,29 @@ import arc.scene.style.Drawable;
 import arc.scene.ui.layout.Table;
 import mindustry.ui.Styles;
 
-public class Function extends Table{
+public class Function extends Table {
     public final String name;
     public final Drawable icon;
-    public final Runnable listener;
     public final String labelName;
 
 
-    public Function(String name, Drawable icon, Runnable listener, String labelName) {
+    public Function(String name, Drawable icon, String labelName) {
         this.name = name;
         this.icon = icon;
         this.labelName = labelName;
-        this.listener = listener;
     }
 
     public Table setTable() {
         return new Table(t -> {
             t.name = name;
-            t.button(icon, listener).size(50f).tooltip(tt -> {
+            t.button(icon, this::onClick).size(50f).tooltip(tt -> {
                 tt.setBackground(Styles.black6);
                 tt.label(() -> labelName).pad(2f);
             });
         });
+    }
+
+    public void onClick() {
+
     }
 }
