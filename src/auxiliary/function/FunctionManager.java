ceda97b74419afc2e5f38b4fcf6fe9a67dc2e528
Ancvike <2047870696@qq.com>
2025-01-09 22:36:03 +0800

    测试

--- src/auxiliary/function/FunctionManager.java
+++ src/auxiliary/function/FunctionManager.java
@@ -7,6 +7,7 @@ import mindustry.Vars;
 
 import mindustry.game.EventType;
 import mindustry.ui.Styles;
+import mindustry.ui.dialogs.BaseDialog;
 
 import static mindustry.Vars.mobile;
 
@@ -45,30 +46,36 @@ public class FunctionManager {
             Events.run(EventType.Trigger.uiDrawEnd, () -> {
                 if (Core.settings.getBool("landscape") && Vars.ui.hudGroup.find("auxiliary-functions") != null) {
                     Vars.ui.hudGroup.removeChild(Vars.ui.hudGroup.find("auxiliary-functions"));
-                    Vars.ui.hudGroup.fill(t -> {
-                        t.name = "auxiliary-functions-mobile-landscape";
-                        for (Function function : functions) {
-                            t.add(function.setTable()).size(50f).tooltip(tt -> {
-                                tt.setBackground(Styles.black6);
-                                tt.label(() -> function.labelName).pad(2f);
-                            });
-                        }
-                        t.bottom();
-                        t.right();
-                    });
-                } else {
+//                    Vars.ui.hudGroup.fill(t -> {
+//                        t.name = "auxiliary-functions-mobile-landscape";
+//                        for (Function function : functions) {
+//                            t.add(function.setTable()).size(50f).tooltip(tt -> {
+//                                tt.setBackground(Styles.black6);
+//                                tt.label(() -> function.labelName).pad(2f);
+//                            });
+//                        }
+//                        t.bottom();
+//                        t.right();
+//                    });
+                    BaseDialog dialog = new BaseDialog("s->h");
+                    dialog.addCloseButton();
+                    dialog.show();
+                } else if (!Core.settings.getBool("landscape") && Vars.ui.hudGroup.find("auxiliary-functions-mobile-landscape") != null) {
                     Vars.ui.hudGroup.removeChild(Vars.ui.hudGroup.find("auxiliary-functions-mobile-landscape"));
-                    Vars.ui.hudGroup.fill(t -> {
-                        t.name = "auxiliary-functions";
-                        for (Function function : functions) {
-                            t.add(function.setTable()).size(50f).tooltip(tt -> {
-                                tt.setBackground(Styles.black6);
-                                tt.label(() -> function.labelName).pad(2f);
-                            });
-                            t.row();
-                        }
-                        t.right();
-                    });
+//                    Vars.ui.hudGroup.fill(t -> {
+//                        t.name = "auxiliary-functions";
+//                        for (Function function : functions) {
+//                            t.add(function.setTable()).size(50f).tooltip(tt -> {
+//                                tt.setBackground(Styles.black6);
+//                                tt.label(() -> function.labelName).pad(2f);
+//                            });
+//                            t.row();
+//                        }
+//                        t.right();
+//                    });
+                    BaseDialog dialog = new BaseDialog("h->s");
+                    dialog.addCloseButton();
+                    dialog.show();
                 }
             });
         }
