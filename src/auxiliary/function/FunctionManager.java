3cb84034d5d85287f23bb9bbf247020c3ee395a8
Ancvike <2047870696@qq.com>
2025-01-09 15:33:13 +0800

    测试

--- src/auxiliary/function/FunctionManager.java
+++ src/auxiliary/function/FunctionManager.java
@@ -1,15 +1,9 @@
 package auxiliary.function;
 
 import arc.Core;
-import arc.Events;
-import arc.input.KeyCode;
-import arc.scene.event.ClickListener;
-import arc.scene.event.InputEvent;
 import arc.struct.Seq;
 import mindustry.Vars;
-import mindustry.game.EventType;
 import mindustry.ui.Styles;
-import mindustry.ui.dialogs.BaseDialog;
 
 import static mindustry.Vars.mobile;
 
@@ -35,21 +29,6 @@ public class FunctionManager {
                 t.right();
             }
         });
-        check();
-    }
-
-    public static void check() {
-        if (!mobile) {
-            Events.on(EventType.WorldLoadEvent.class, e -> Vars.ui.hudGroup.addListener(new ClickListener() {
-                @Override
-                public boolean touchDown(InputEvent event, float x, float y, int pointer, KeyCode button) {
-                    BaseDialog dialog = new BaseDialog("");
-                    dialog.addCloseButton();
-                    dialog.show();
-                    return false;
-                }
-            }));
-        }
     }
 }
 
