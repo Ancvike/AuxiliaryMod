911507a4073d2a0bc2284d5566b60b0aaec2fea3
Ancvike <2047870696@qq.com>
2025-01-09 11:28:57 +0800

    测试

--- src/auxiliary/function/FunctionManager.java
+++ src/auxiliary/function/FunctionManager.java
@@ -7,6 +7,7 @@ import arc.util.Time;
 import mindustry.Vars;
 import mindustry.game.EventType;
 import mindustry.ui.Styles;
+import mindustry.ui.dialogs.BaseDialog;
 
 import static mindustry.Vars.mobile;
 
@@ -38,24 +39,27 @@ public class FunctionManager {
                 time += Time.delta;
                 if (time > 1f) {
                     time = 0;
-                    if (Core.settings.getBool("landscape")) {
-                        Vars.ui.hudGroup.removeChild(Vars.ui.hudGroup.find("auxiliary-functions"));
-                        Vars.ui.hudGroup.fill(t -> {
-                            t.name = "auxiliary-functions";
-                            for (Function function : functions) {
-                                t.add(function.setTable()).size(50f).tooltip(tt -> {
-                                    tt.setBackground(Styles.black6);
-                                    tt.label(() -> function.labelName).pad(2f);
-                                });
-                                if (!(mobile && Core.settings.getBool("landscape"))) t.row();
-                            }
-                            if (mobile && Core.settings.getBool("landscape")) {
-                                t.bottom();
-                            } else {
-                                t.right();
-                            }
-                        });
-                    }
+                    BaseDialog dialog = new BaseDialog("");
+                    dialog.addCloseButton();
+                    dialog.show();
+//                    if (Core.settings.getBool("landscape")) {
+//                        Vars.ui.hudGroup.removeChild(Vars.ui.hudGroup.find("auxiliary-functions"));
+//                        Vars.ui.hudGroup.fill(t -> {
+//                            t.name = "auxiliary-functions";
+//                            for (Function function : functions) {
+//                                t.add(function.setTable()).size(50f).tooltip(tt -> {
+//                                    tt.setBackground(Styles.black6);
+//                                    tt.label(() -> function.labelName).pad(2f);
+//                                });
+//                                if (!(mobile && Core.settings.getBool("landscape"))) t.row();
+//                            }
+//                            if (mobile && Core.settings.getBool("landscape")) {
+//                                t.bottom();
+//                            } else {
+//                                t.right();
+//                            }
+//                        });
+//                    }
                 }
             });
         }
