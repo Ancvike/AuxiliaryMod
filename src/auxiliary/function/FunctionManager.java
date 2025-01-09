79552fe89fec67ddb3f55365a843e2a391f79d79
Ancvike <2047870696@qq.com>
2025-01-09 13:31:12 +0800

    测试

--- src/auxiliary/function/FunctionManager.java
+++ src/auxiliary/function/FunctionManager.java
@@ -5,12 +5,9 @@ import arc.Events;
 import arc.input.KeyCode;
 import arc.scene.event.ClickListener;
 import arc.scene.event.InputEvent;
-import arc.scene.event.InputListener;
 import arc.struct.Seq;
-import arc.util.Time;
 import mindustry.Vars;
 import mindustry.game.EventType;
-import mindustry.input.MobileInput;
 import mindustry.ui.Styles;
 import mindustry.ui.dialogs.BaseDialog;
 
@@ -43,17 +40,15 @@ public class FunctionManager {
 
     public static void check() {
         if (!mobile) {
-            Events.on(EventType.WorldLoadEvent.class, e -> {
-                Vars.ui.hudGroup.addListener(new ClickListener() {
-                    @Override
-                    public boolean touchDown(InputEvent event, float x, float y, int pointer, KeyCode button) {
-                        BaseDialog dialog = new BaseDialog("");
-                        dialog.addCloseButton();
-                        dialog.show();
-                        return false;
-                    }
-                });
-            });
+            Events.on(EventType.WorldLoadEvent.class, e -> Vars.ui.hudGroup.addListener(new ClickListener() {
+                @Override
+                public boolean touchDown(InputEvent event, float x, float y, int pointer, KeyCode button) {
+                    BaseDialog dialog = new BaseDialog("");
+                    dialog.addCloseButton();
+                    dialog.show();
+                    return false;
+                }
+            }));
         }
     }
 }
