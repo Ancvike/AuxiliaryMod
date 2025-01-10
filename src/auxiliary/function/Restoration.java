a8d18602babba1de4f42285298f071eaf1461e98
Ancvike <2047870696@qq.com>
2025-01-10 20:37:56 +0800

    测试

--- src/auxiliary/function/Restoration.java
+++ src/auxiliary/function/Restoration.java
@@ -63,7 +63,7 @@ public class Restoration extends Function {
 
     private void restoration_unit() {
         for (Unit unit : Team.sharded.data().units) {
-            unit.health = unit.maxHealth / 2;
+            unit.health = unit.maxHealth;
         }
     }
 }
