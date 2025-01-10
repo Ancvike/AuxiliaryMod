68e61e9fd4d7b65dac5cff29ab5b0859a3f7f717
Ancvike <2047870696@qq.com>
2025-01-10 21:13:48 +0800

    测试

--- src/auxiliary/function/UIMovement.java
+++ src/auxiliary/function/UIMovement.java
@@ -2,11 +2,18 @@ package auxiliary.function;
 
 import arc.Core;
 import arc.graphics.Color;
+import arc.input.KeyCode;
+import arc.math.geom.Vec2;
+import arc.scene.event.InputEvent;
+import arc.scene.event.InputListener;
+import arc.scene.event.Touchable;
 import arc.scene.ui.TextField;
 import arc.scene.ui.layout.Table;
 import arc.struct.Seq;
+import arc.util.Tmp;
 import mindustry.Vars;
 import mindustry.gen.Icon;
+import mindustry.ui.Styles;
 import mindustry.ui.dialogs.BaseDialog;
 
 import static auxiliary.dialogs.Dialogs.setDialog_yes;
@@ -27,14 +34,28 @@ public class UIMovement extends Function {
 
     @Override
     public void onClick() {
-        if (mobile && !Core.settings.getBool("landscape")){
+        if (mobile && !Core.settings.getBool("landscape")) {
             table = setDialogTable_mobile();
-        }else {
+        } else {
             table = setDialogTable();
         }
         setDialog_yes(dialog_movement, table);
         dialog_movement.show();
     }
+
+    @Override
+    public Table setTable() {
+        return new Table(t -> {
+            t.name = name;
+            t.button(icon, this::onClick).size(50f).tooltip(tt -> {
+                tt.setBackground(Styles.black6);
+                tt.label(() -> labelName).pad(2f);
+            });
+            t.touchable = Touchable.enabled;
+            t.addListener(new DragHandleListener(FunctionManager.table));
+        });
+    }
+
     public Table setDialogTable_mobile() {
         return new Table(t -> {
             t.add("UI移动界面").row();
@@ -55,6 +76,7 @@ public class UIMovement extends Function {
             t.button("重置", this::click_reset).size(120f, 50f).row();
         });
     }
+
     public Table setDialogTable() {
         return new Table(t -> {
             t.add("UI移动界面");
@@ -156,5 +178,31 @@ public class UIMovement extends Function {
     }
 
     private void yText() {
+    }
+}
+
+class DragHandleListener extends InputListener {
+    protected float lastX, lastY;
+    final Table table;
+
+    public DragHandleListener(Table table) {
+        this.table = table;
+    }
+
+    @Override
+    public boolean touchDown(InputEvent event, float x, float y, int pointer, KeyCode button) {
+        Vec2 v = event.listenerActor.localToStageCoordinates(Tmp.v1.set(x, y));
+        lastX = v.x;
+        lastY = v.y;
+        table.toFront();
+        return true;
+    }
+
+    @Override
+    public void touchDragged(InputEvent event, float dx, float dy, int pointer) {
+        Vec2 v = event.listenerActor.localToStageCoordinates(Tmp.v1.set(dx, dy));
+        table.setPosition( table.x + (v.x - lastX),  table.y + (v.y - lastY));
+        lastX = v.x;
+        lastY = v.y;
     }
 }
