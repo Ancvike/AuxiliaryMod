54975271b385eed568e7f9bc41e23d26909c5436
Ancvike <2047870696@qq.com>
2025-01-11 21:56:03 +0800

    测试

--- src/auxiliary/binding/KeyBind_Keyboard.java
+++ src/auxiliary/binding/KeyBind_Keyboard.java
@@ -2,6 +2,7 @@ package auxiliary.binding;
 
 import arc.Core;
 import arc.Events;
+import mindustry.Vars;
 import mindustry.game.EventType;
 import mindustry.ui.dialogs.BaseDialog;
 
@@ -12,13 +13,15 @@ public class KeyBind_Keyboard {
 
     public static void init() {
         Events.run(EventType.Trigger.uiDrawEnd, () -> {
-            if (Core.input.keyDown(CONVEYOR_CHANGE.nowKeyCode) && !is) {
-                BaseDialog dialog = new BaseDialog("传送带升级");
-                dialog.addCloseButton();
-                dialog.show();
-                is = true;
-            } else if (Core.input.keyRelease(CONVEYOR_CHANGE.nowKeyCode)) {
-                is = false;
+            if (Vars.state.isGame()) {
+                if (Core.input.keyDown(CONVEYOR_CHANGE.nowKeyCode) && !is) {
+                    BaseDialog dialog = new BaseDialog("传送带升级");
+                    dialog.addCloseButton();
+                    dialog.show();
+                    is = true;
+                } else if (Core.input.keyRelease(CONVEYOR_CHANGE.nowKeyCode)) {
+                    is = false;
+                }
             }
         });
     }
