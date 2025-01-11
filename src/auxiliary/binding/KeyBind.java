42b49e6455aa2241fe3e021e94b264739ddbb49a
Ancvike <2047870696@qq.com>
2025-01-11 14:55:38 +0800

    测试

--- src/auxiliary/binding/KeyBind.java
+++ src/auxiliary/binding/KeyBind.java
@@ -12,17 +12,17 @@ public class KeyBind {
     private static boolean dialogShown = false;  // 添加一个标志变量来跟踪对话框是否已显示
 
     public static void init() {
-        if (Vars.state.isGame()) {
-            Events.run(EventType.Trigger.uiDrawEnd, () -> {
-                if (input.keyDown(CONVEYOR_CHANGE.nowKeyCode) && !dialogShown) {  // 检查键是否被按下且对话框未显示
-                    BaseDialog dialog = new BaseDialog("传送带升级");
-                    dialog.addCloseButton();
-                    dialog.show();
-                    dialogShown = true;  // 设置对话框已显示的标志
-                } else if (input.keyRelease(CONVEYOR_CHANGE.nowKeyCode)) {  // 检查键是否被释放
-                    dialogShown = false;  // 重置对话框已显示的标志
-                }
-            });
-        }
+
+        Events.run(EventType.Trigger.uiDrawEnd, () -> {
+            if (input.keyDown(CONVEYOR_CHANGE.nowKeyCode) && !dialogShown) {  // 检查键是否被按下且对话框未显示
+                BaseDialog dialog = new BaseDialog("传送带升级");
+                dialog.addCloseButton();
+                dialog.show();
+                dialogShown = true;  // 设置对话框已显示的标志
+            } else if (input.keyRelease(CONVEYOR_CHANGE.nowKeyCode)) {  // 检查键是否被释放
+                dialogShown = false;  // 重置对话框已显示的标志
+            }
+        });
+
     }
 }
