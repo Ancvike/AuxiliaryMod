fc800c9919ca6e9ead12bac8eacbc1a7b8fc110f
Ancvike <2047870696@qq.com>
2025-01-11 22:14:03 +0800

    测试

--- src/auxiliary/binding/KeyBind_Keyboard.java
+++ src/auxiliary/binding/KeyBind_Keyboard.java
@@ -4,14 +4,15 @@ import arc.Core;
 import arc.Events;
 import mindustry.Vars;
 import mindustry.game.EventType;
+import mindustry.input.InputHandler;
 import mindustry.ui.dialogs.BaseDialog;
 
 import static auxiliary.binding.MyKeyBind.CONVEYOR_CHANGE;
 
-public class KeyBind_Keyboard {
+public class KeyBind_Keyboard extends InputHandler {
     public static boolean is = false;
 
-    public static void init() {
+    public void init() {
         Events.run(EventType.Trigger.uiDrawEnd, () -> {
             if (Vars.state.isGame()) {
                 if (Core.input.keyDown(CONVEYOR_CHANGE.nowKeyCode) && !is) {
@@ -20,6 +21,15 @@ public class KeyBind_Keyboard {
                     dialog.show();
                     is = true;
                 } else if (Core.input.keyRelease(CONVEYOR_CHANGE.nowKeyCode)) {
+                    is = false;
+                }
+
+                if (Core.input.keyDown(MyKeyBind.RECOVERY_UNIT.nowKeyCode) && commandMode) {
+                    BaseDialog dialog = new BaseDialog("建筑修复");
+                    dialog.addCloseButton();
+                    dialog.show();
+                    is = true;
+                } else if (Core.input.keyRelease(MyKeyBind.RECOVERY_UNIT.nowKeyCode)) {
                     is = false;
                 }
             }
