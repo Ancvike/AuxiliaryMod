30dfac46e6e34e5f7214f8196f099cffae262a17
Ancvike <2047870696@qq.com>
2025-01-11 22:14:03 +0800

    测试

--- src/auxiliary/AuxiliaryMod.java
+++ src/auxiliary/AuxiliaryMod.java
@@ -13,7 +13,7 @@ public class AuxiliaryMod extends Mod {
         Events.on(EventType.ClientLoadEvent.class, e -> {
             Dialogs.init();
             SettingUI.init();
-            KeyBind_Keyboard.init();
+            new KeyBind_Keyboard().init();
             FunctionManager.init();
         });
     }
