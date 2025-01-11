7bd2f01c85416361e1c6261e61e0cb8128b1ba5d
Ancvike <2047870696@qq.com>
2025-01-11 14:52:34 +0800

    测试

--- src/auxiliary/binding/KeyBind.java
+++ src/auxiliary/binding/KeyBind.java
@@ -9,13 +9,18 @@ import static arc.Core.input;
 import static auxiliary.binding.MyKeyBind.CONVEYOR_CHANGE;
 
 public class KeyBind {
+    private static boolean dialogShown = false;  // 添加一个标志变量来跟踪对话框是否已显示
+
     public static void init() {
         if (Vars.state.isGame()) {
             Events.run(EventType.Trigger.uiDrawEnd, () -> {
-                if (input.keyDown(CONVEYOR_CHANGE.nowKeyCode)) {
+                if (input.keyDown(CONVEYOR_CHANGE.nowKeyCode) && !dialogShown) {  // 检查键是否被按下且对话框未显示
                     BaseDialog dialog = new BaseDialog("传送带升级");
                     dialog.addCloseButton();
                     dialog.show();
+                    dialogShown = true;  // 设置对话框已显示的标志
+                } else if (input.keyRelease(CONVEYOR_CHANGE.nowKeyCode)) {  // 检查键是否被释放
+                    dialogShown = false;  // 重置对话框已显示的标志
                 }
             });
         }
