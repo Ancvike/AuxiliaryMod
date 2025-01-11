c477f21d199e0541e3f129688bf1cbaf436f4f63
Ancvike <2047870696@qq.com>
2025-01-11 23:10:49 +0800

    测试

--- src/auxiliary/binding/KeyBind_Keyboard.java
+++ src/auxiliary/binding/KeyBind_Keyboard.java
@@ -2,27 +2,41 @@ package auxiliary.binding;
 
 import arc.Core;
 import arc.Events;
+import arc.math.geom.Vec2;
 import arc.struct.Seq;
 import mindustry.Vars;
+import mindustry.core.World;
 import mindustry.game.EventType;
 import mindustry.gen.Unit;
-import mindustry.ui.dialogs.BaseDialog;
+import mindustry.input.Binding;
+import mindustry.input.InputHandler;
 
 import static auxiliary.binding.MyKeyBind.CONVEYOR_CHANGE;
+import static auxiliary.binding.MyKeyBind.RECOVERY_BUDDING;
 
-public class KeyBind_Keyboard {
+public class KeyBind_Keyboard extends InputHandler {
     public static boolean is = false;
 
     public void init() {
         Events.run(EventType.Trigger.uiDrawEnd, () -> {
             if (Vars.state.isGame()) {
-                if (Core.input.keyDown(CONVEYOR_CHANGE.nowKeyCode) && !is) {
-                    BaseDialog dialog = new BaseDialog("传送带升级");
-                    dialog.addCloseButton();
-                    dialog.show();
-                    is = true;
-                } else if (Core.input.keyRelease(CONVEYOR_CHANGE.nowKeyCode)) {
-                    is = false;
+                int selectX = -1;
+                int selectY = -1;
+                if (Core.input.keyTap(RECOVERY_BUDDING.nowKeyCode)) {
+                    selectX = tileX(Core.input.mouseX());
+                    selectY = tileY(Core.input.mouseY());
+                    int schemX = World.toTile(Core.input.mouseWorld().x);
+                    int schemY = World.toTile(Core.input.mouseWorld().y);
+                } else if (Core.input.keyRelease(RECOVERY_BUDDING.nowKeyCode)) {
+                    int cursorX = tileX(Core.input.mouseX());
+                    int cursorY = tileY(Core.input.mouseY());
+                    removeSelection(selectX, selectY, cursorX, cursorY, !Core.input.keyDown(Binding.schematic_select) ? 100 : Vars.maxSchematicSize);
+                    if (lastSchematic != null) {
+                        useSchematic(lastSchematic);
+                        lastSchematic = null;
+                    }
+                    selectX = -1;
+                    selectY = -1;
                 }
 
                 if (Core.input.keyDown(MyKeyBind.RECOVERY_UNIT.nowKeyCode) && Vars.control.input.commandMode && !is) {
@@ -37,5 +51,21 @@ public class KeyBind_Keyboard {
                 }
             }
         });
+    }
+
+    int tileX(float cursorX) {
+        Vec2 vec = Core.input.mouseWorld(cursorX, 0);
+        if (selectedBlock()) {
+            vec.sub(block.offset, block.offset);
+        }
+        return World.toTile(vec.x);
+    }
+
+    int tileY(float cursorY) {
+        Vec2 vec = Core.input.mouseWorld(0, cursorY);
+        if (selectedBlock()) {
+            vec.sub(block.offset, block.offset);
+        }
+        return World.toTile(vec.y);
     }
 }
