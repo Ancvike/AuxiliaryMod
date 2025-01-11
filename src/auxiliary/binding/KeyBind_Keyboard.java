cbc13350d33abc5fe7f4e40bd47db20ef2c1a8db
Ancvike <2047870696@qq.com>
2025-01-11 21:39:16 +0800

    测试

--- src/auxiliary/binding/KeyBind_Keyboard.java
+++ src/auxiliary/binding/KeyBind_Keyboard.java
@@ -1,8 +1,25 @@
 package auxiliary.binding;
 
+import arc.Core;
+import arc.Events;
+import mindustry.game.EventType;
+import mindustry.ui.dialogs.BaseDialog;
+
+import static auxiliary.binding.MyKeyBind.CONVEYOR_CHANGE;
+
 public class KeyBind_Keyboard {
+    public static boolean is = false;
 
-    public void init() {
-
+    public static void init() {
+        Events.run(EventType.Trigger.uiDrawEnd, () -> {
+            if (Core.input.keyDown(CONVEYOR_CHANGE.nowKeyCode) && !is) {
+                BaseDialog dialog = new BaseDialog("传送带升级");
+                dialog.addCloseListener();
+                dialog.show();
+                is = true;
+            } else if (Core.input.keyRelease(CONVEYOR_CHANGE.nowKeyCode)) {
+                is = false;
+            }
+        });
     }
 }
