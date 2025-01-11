c17e22c9d8e664726ef4a65759bd23ebe928500e
Ancvike <2047870696@qq.com>
2025-01-11 20:54:55 +0800

    测试

--- src/auxiliary/binding/KeyBind_Keyboard.java
+++ src/auxiliary/binding/KeyBind_Keyboard.java
@@ -1,52 +1,8 @@
 package auxiliary.binding;
 
-import arc.Core;
-import arc.Events;
-import arc.math.geom.Vec2;
-import mindustry.core.World;
-import mindustry.game.EventType;
-import mindustry.input.InputHandler;
-
-import static arc.Core.input;
-import static auxiliary.binding.MyKeyBind.CONVEYOR_CHANGE;
-import static mindustry.Vars.state;
-
-public class KeyBind_Keyboard extends InputHandler {
-    public int schemX = -1, schemY = -1;
+public class KeyBind_Keyboard {
 
     public void init() {
-        Events.run(EventType.Trigger.uiDrawEnd, () -> {
-//            if (state.isGame()) {
-                if (Core.input.keyDown(CONVEYOR_CHANGE.nowKeyCode)) {
-                    schemX = World.toTile(Core.input.mouseWorld().x);
-                    schemY = World.toTile(Core.input.mouseWorld().y);
-                    int cursorX = tileX(Core.input.mouseX());
-                    int cursorY = tileY(Core.input.mouseY());
-                    drawRebuildSelection(schemX, schemY, cursorX, cursorY);
-                } else if (input.keyRelease(CONVEYOR_CHANGE.nowKeyCode)) {
-                    int rawCursorX = World.toTile(Core.input.mouseWorld().x);
-                    int rawCursorY = World.toTile(Core.input.mouseWorld().y);
-                    rebuildArea(schemX, schemY, rawCursorX, rawCursorY);
-                    schemX = -1;
-                    schemY = -1;
-                }
-//            }
-        });
-    }
 
-    int tileX(float cursorX) {
-        Vec2 vec = Core.input.mouseWorld(cursorX, 0);
-        if (selectedBlock()) {
-            vec.sub(block.offset, block.offset);
-        }
-        return World.toTile(vec.x);
-    }
-
-    int tileY(float cursorY) {
-        Vec2 vec = Core.input.mouseWorld(0, cursorY);
-        if (selectedBlock()) {
-            vec.sub(block.offset, block.offset);
-        }
-        return World.toTile(vec.y);
     }
 }
