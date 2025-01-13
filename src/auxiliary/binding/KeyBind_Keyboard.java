3841ec578c7e9b85f8adc4faea4800a26fb7151b
Ancvike <2047870696@qq.com>
2025-01-13 16:25:22 +0800

    测试

--- src/auxiliary/binding/KeyBind_Keyboard.java
+++ src/auxiliary/binding/KeyBind_Keyboard.java
@@ -25,7 +25,7 @@ public class KeyBind_Keyboard extends InputHandler {
         Events.run(EventType.Trigger.update, () -> {
             if (!Core.scene.hasKeyboard() && startX != 0 && startY != 0 && endX != 0 && endY != 0) {
                 if (Core.input.keyRelease(RECOVERY_BUDDING.nowKeyCode)) {
-                    rebuildArea(startX, startY, endX, endY);
+                    rebuildArea(endX, endY, startX, startY);
                     startX = 0;
                     startY = 0;
 //                    Vars.ui.hudfrag.showToast("所选建筑已修复");
@@ -40,17 +40,10 @@ public class KeyBind_Keyboard extends InputHandler {
 
         Events.run(EventType.Trigger.update, () -> {
             if (Core.input.keyDown(RECOVERY_BUDDING.nowKeyCode)) {
-//                Lines.stroke(1f);
+
                 endX = World.toTile(Core.input.mouseWorld().x);
                 endY = World.toTile(Core.input.mouseWorld().y);
-                Placement.NormalizeDrawResult result = Placement.normalizeDrawArea(Blocks.air, startX, startY, endX, endY, false, 0, 1f);
-
-                Lines.stroke(2f);
-                Vars.ui.hudfrag.showToast("所选建筑已修复");
-                Draw.color(Pal.darkerGray);
-                Lines.rect(result.x, result.y - 1, result.x2 - result.x, result.y2 - result.y);
-                Draw.color(Pal.gray);
-                Lines.rect(result.x, result.y, result.x2 - result.x, result.y2 - result.y);
+                drawRebuildSelection(endX, endY, startX, startY);
             }
         });
 
