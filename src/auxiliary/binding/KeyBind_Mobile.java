a0b934358904d7411e400f39ec0fdbf6733c48cd
Ancvike <2047870696@qq.com>
2025-01-13 22:27:59 +0800

    测试

---
+++ src/auxiliary/binding/KeyBind_Mobile.java
@@ -0,0 +1,34 @@
+package auxiliary.binding;
+
+import arc.Events;
+import arc.struct.Seq;
+import mindustry.Vars;
+import mindustry.game.EventType;
+import mindustry.gen.Icon;
+import mindustry.gen.Unit;
+import mindustry.ui.Styles;
+
+
+public class KeyBind_Mobile {
+    public void init() {
+        Events.run(EventType.Trigger.update, () -> {
+            if (Vars.control.input.commandMode) {
+                Vars.ui.hudGroup.fill(t -> {
+                    t.name = "mobile-recovery-unit";
+                    t.button(Icon.android, this::onClick).size(50f).tooltip(tt -> {
+                        tt.setBackground(Styles.black6);
+                        tt.label(() -> "单位修复").pad(2f);
+                    });
+                });
+            }
+        });
+    }
+
+    private void onClick() {
+        Seq<Unit> selectedUnits = Vars.control.input.selectedUnits;
+        for (Unit unit : selectedUnits) {
+            unit.health = unit.maxHealth;
+        }
+        Vars.ui.hudfrag.showToast("所选单位已修复");
+    }
+}
