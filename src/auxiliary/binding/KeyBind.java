792b581513219465bb20dd407dc49d3d004b3b08
Ancvike <2047870696@qq.com>
2025-01-16 22:24:15 +0800

    测试

--- src/auxiliary/binding/KeyBind.java
+++ src/auxiliary/binding/KeyBind.java
@@ -3,7 +3,7 @@ package auxiliary.binding;
 import static mindustry.Vars.mobile;
 
 public class KeyBind {
-    public static KeyBind_Keyboard keyBindKeyboard;
+    public static KeyBind_Keyboard keyBindKeyboard = new KeyBind_Keyboard();
     public static void init() {
         if (mobile) {
             new KeyBind_Mobile().init();
