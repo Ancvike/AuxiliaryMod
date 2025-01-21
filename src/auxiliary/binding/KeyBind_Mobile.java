7dab72d6975ce168c707c4ff65b6a463de5fe414
Ancvike <2047870696@qq.com>
2025-01-21 10:40:05 +0800

    测试

--- src/auxiliary/binding/KeyBind_Mobile.java
+++ src/auxiliary/binding/KeyBind_Mobile.java
@@ -12,23 +12,13 @@ import mindustry.gen.Icon;
 import mindustry.gen.Unit;
 import mindustry.input.InputHandler;
 import mindustry.ui.Styles;
-import mindustry.ui.dialogs.BaseDialog;
 
 public class KeyBind_Mobile extends InputHandler implements GestureDetector.GestureListener {
     boolean isUnitTrue = false;
     int count = 0;
 
     public void init() {
-        Vars.ui.hudGroup.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, KeyCode button) {
-                BaseDialog dialog =new BaseDialog("");
-                dialog.addCloseButton();
-                dialog.show();
-                return false;
-            }
 
-        });
         Events.run(EventType.Trigger.uiDrawEnd, () -> {
             isUnitTrue = Vars.control.input.commandMode;
             if (isUnitTrue && count == 0) {
