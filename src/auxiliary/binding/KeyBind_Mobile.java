8c8591c62eea7b2e888e83e46338993e600e9b3a
Ancvike <2047870696@qq.com>
2025-01-21 19:40:14 +0800

    测试

--- src/auxiliary/binding/KeyBind_Mobile.java
+++ src/auxiliary/binding/KeyBind_Mobile.java
@@ -4,9 +4,12 @@ import arc.Core;
 import arc.Events;
 import arc.graphics.g2d.Draw;
 import arc.graphics.g2d.Lines;
+import arc.input.KeyCode;
 import arc.math.Mathf;
+import arc.math.geom.Geometry;
 import arc.math.geom.Vec2;
 import arc.scene.ui.layout.Scl;
+import arc.scene.ui.layout.Table;
 import arc.struct.Seq;
 import mindustry.Vars;
 import mindustry.content.Blocks;
@@ -16,47 +19,75 @@ import mindustry.gen.Icon;
 import mindustry.gen.Unit;
 import mindustry.graphics.Layer;
 import mindustry.graphics.Pal;
-import mindustry.input.InputHandler;
 import mindustry.ui.Styles;
 import mindustry.world.Block;
 import mindustry.world.Tile;
 
+import static auxiliary.function.KeyBind_Mobile_Function.isClick;
+import static mindustry.Vars.mobile;
 import static mindustry.Vars.world;
 
-public class KeyBind_Mobile extends InputHandler {
+public class KeyBind_Mobile extends Table {
     boolean isUnitTrue = false;
     int count = 0;
 
-    public static Block drawBlock = Blocks.router;
     EditorTool tool;
     final Vec2[][] brushPolygons = new Vec2[MapEditor.brushSizes.length][0];
+    public static Block drawBlock = Blocks.router;
+    boolean drawing;
+    int lastx, lasty;
 
     public void init() {
 
+        for (int i = 0; i < MapEditor.brushSizes.length; i++) {
+            float size = MapEditor.brushSizes[i];
+            float mod = size % 1f;
+            brushPolygons[i] = Geometry.pixelCircle(size, (index, x, y) -> Mathf.dst(x, y, index - mod, index - mod) <= size - 0.5f);
+        }
+
         Events.run(EventType.Trigger.draw, () -> {
-            float cx = Core.camera.position.x, cy = Core.camera.position.y;
-            float scaling = 8;
+            if (isClick) {
+                float cx = Core.camera.position.x, cy = Core.camera.position.y;
+                float scaling = 8;
 
-            Draw.z(Layer.max);
+                Draw.z(Layer.max);
 
-            Lines.stroke(1f);
-            Draw.color(Pal.accent);
-            for (int i = (int) (-0.5f * Core.camera.height / 8); i < (int) (0.5f * Core.camera.height / 8); i++) {
-                Lines.line(Mathf.floor((cx - 0.5f * Core.camera.width) / 8) * 8 + 4, Mathf.floor((cy + i * 8) / 8) * 8 + 4, Mathf.floor((cx + 0.5f * Core.camera.width) / 8) * 8 + 4, Mathf.floor((cy + i * 8) / 8) * 8 + 4);
-            }
-            for (int i = (int) (-0.5f * Core.camera.width / 8); i < (int) (0.5f * Core.camera.width / 8); i++) {
-                Lines.line(Mathf.floor((cx + i * 8) / 8) * 8 + 4, Mathf.floor((cy + 0.5f * Core.camera.height) / 8) * 8 + 4, Mathf.floor((cx + i * 8) / 8) * 8 + 4, Mathf.floor((cy - 0.5f * Core.camera.height) / 8) * 8 + 4);
-            }
-            Draw.reset();
-
-            Tile tile = world.tileWorld(Core.input.mouseWorldX(), Core.input.mouseWorldY());
-
-            Lines.stroke(Scl.scl(2f), Pal.accent);
-
-            if (!drawBlock.isMultiblock() || tool == EditorTool.eraser) {
-                if (tool.edit) {
-                    Lines.poly(brushPolygons[2], tile.x * 8 - 4, tile.y * 8 - 4, scaling);
+                if (Core.settings.getBool("grid")) {
+                    Lines.stroke(1f);
+                    Draw.color(Pal.accent);
+                    for (int i = (int) (-0.5f * Core.camera.height / 8); i < (int) (0.5f * Core.camera.height / 8); i++) {
+                        Lines.line(Mathf.floor((cx - 0.5f * Core.camera.width) / 8) * 8 + 4, Mathf.floor((cy + i * 8) / 8) * 8 + 4, Mathf.floor((cx + 0.5f * Core.camera.width) / 8) * 8 + 4, Mathf.floor((cy + i * 8) / 8) * 8 + 4);
+                    }
+                    for (int i = (int) (-0.5f * Core.camera.width / 8); i < (int) (0.5f * Core.camera.width / 8); i++) {
+                        Lines.line(Mathf.floor((cx + i * 8) / 8) * 8 + 4, Mathf.floor((cy + 0.5f * Core.camera.height) / 8) * 8 + 4, Mathf.floor((cx + i * 8) / 8) * 8 + 4, Mathf.floor((cy - 0.5f * Core.camera.height) / 8) * 8 + 4);
+                    }
+                    Draw.reset();
                 }
+
+                Tile tile = world.tileWorld(Core.input.mouseWorldX(), Core.input.mouseWorldY());
+
+                Lines.stroke(Scl.scl(2f), Pal.accent);
+
+                if (!drawBlock.isMultiblock() || tool == EditorTool.eraser) {
+                    if (!mobile || drawing) {
+                        Lines.poly(brushPolygons[2], tile.x * 8 - 4, tile.y * 8 - 4, scaling);
+                    }
+                }
+            }
+        });
+
+        Events.run(EventType.Trigger.update, () -> {
+            Tile tile = world.tileWorld(Core.input.mouseWorldX(), Core.input.mouseWorldY());
+            if (tile == null || drawBlock == null || hasMouse()) return;
+            if (Core.input.isTouched()) {
+                if (!mobile && !Core.input.keyDown(KeyCode.mouseLeft)) return;
+                drawing = true;
+                lastx = tile.x;
+                lasty = tile.y;
+            } else {
+                drawing = false;
+                lastx = -1;
+                lasty = -1;
             }
         });
 
@@ -97,22 +128,5 @@ public class KeyBind_Mobile extends InputHandler {
 }
 
 enum EditorTool {
-    eraser("eraseores") {
-        {
-            edit = true;
-        }
-    };
-
-    /**
-     * All the internal alternate placement modes of this tool.
-     */
-    public final String[] altModes;
-    /**
-     * Whether this tool causes canvas changes when touched.
-     */
-    public boolean edit;
-
-    EditorTool(String... altModes) {
-        this.altModes = altModes;
-    }
+    eraser() {}
 }
