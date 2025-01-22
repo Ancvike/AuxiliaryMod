973a2cfa49893af7ebff1acbb5e58f6c893aaf4e
Ancvike <2047870696@qq.com>
2025-01-22 15:08:34 +0800

    测试

--- src/auxiliary/binding/KeyBind_Mobile.java
+++ src/auxiliary/binding/KeyBind_Mobile.java
@@ -1,82 +1,19 @@
 package auxiliary.binding;
 
-import arc.Core;
 import arc.Events;
-import arc.graphics.g2d.Draw;
-import arc.graphics.g2d.Lines;
-import arc.input.KeyCode;
-import arc.math.Mathf;
-import arc.math.geom.Geometry;
-import arc.math.geom.Vec2;
-import arc.scene.ui.layout.Scl;
 import arc.scene.ui.layout.Table;
 import arc.struct.Seq;
 import mindustry.Vars;
-import mindustry.content.Blocks;
-import mindustry.editor.MapEditor;
 import mindustry.game.EventType;
 import mindustry.gen.Icon;
 import mindustry.gen.Unit;
-import mindustry.graphics.Layer;
-import mindustry.graphics.Pal;
 import mindustry.ui.Styles;
-import mindustry.world.Block;
-import mindustry.world.Tile;
-
-import static auxiliary.function.KeyBind_Mobile_Function.isClick;
-import static mindustry.Vars.mobile;
-import static mindustry.Vars.world;
 
 public class KeyBind_Mobile extends Table {
     boolean isUnitTrue = false;
     int count = 0;
 
-    EditorTool tool;
-    final Vec2[][] brushPolygons = new Vec2[MapEditor.brushSizes.length][0];
-    public static Block drawBlock = Blocks.router;
-    boolean drawing;
-    int lastx, lasty;
-
     public void init() {
-
-        for (int i = 0; i < MapEditor.brushSizes.length; i++) {
-            float size = MapEditor.brushSizes[i];
-            float mod = size % 1f;
-            brushPolygons[i] = Geometry.pixelCircle(size, (index, x, y) -> Mathf.dst(x, y, index - mod, index - mod) <= size - 0.5f);
-        }
-
-        Events.run(EventType.Trigger.draw, () -> {
-            if (isClick) {
-                float scaling = 8;
-
-                Draw.z(Layer.max);
-
-                Tile tile = world.tileWorld(Core.input.mouseWorldX(), Core.input.mouseWorldY());
-
-                Lines.stroke(Scl.scl(2f), Pal.accent);
-
-                if (!drawBlock.isMultiblock() || tool == EditorTool.eraser) {
-                    if (!mobile || drawing) {
-                        Lines.poly(brushPolygons[2], tile.x * 8 - 4, tile.y * 8 - 4, scaling);
-                    }
-                }
-            }
-        });
-
-        Events.run(EventType.Trigger.update, () -> {
-            Tile tile = world.tileWorld(Core.input.mouseWorldX(), Core.input.mouseWorldY());
-            if (tile == null || drawBlock == null || hasMouse()) return;
-            if (Core.input.isTouched()) {
-                if (!mobile && !Core.input.keyDown(KeyCode.mouseLeft)) return;
-                drawing = true;
-                lastx = tile.x;
-                lasty = tile.y;
-            } else {
-                drawing = false;
-                lastx = -1;
-                lasty = -1;
-            }
-        });
 
         Events.run(EventType.Trigger.uiDrawEnd, () -> {
             isUnitTrue = Vars.control.input.commandMode;
@@ -114,6 +51,3 @@ public class KeyBind_Mobile extends Table {
     }
 }
 
-enum EditorTool {
-    eraser() {}
-}
