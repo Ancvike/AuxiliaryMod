e24c264d0dd9143fc7bc302b8339149fef7241b6
Ancvike <2047870696@qq.com>
2025-01-22 20:36:31 +0800

    测试

--- src/auxiliary/function/KeyBind_Mobile_Function.java
+++ src/auxiliary/function/KeyBind_Mobile_Function.java
@@ -1,6 +1,7 @@
 package auxiliary.function;
 
 import arc.Events;
+import arc.scene.ui.ImageButton;
 import arc.scene.ui.layout.Table;
 import mindustry.game.EventType;
 import mindustry.gen.Icon;
@@ -11,18 +12,18 @@ import static mindustry.Vars.state;
 
 public class KeyBind_Mobile_Function extends Function {
     public static boolean isClick = false;
-    public static boolean isShow = false;
+    ImageButton button = new ImageButton(icon, Styles.clearTogglei);
 
     public KeyBind_Mobile_Function() {
         super("mobile-building-repair", Icon.android, "建筑修复");
-        Events.run(EventType.Trigger.draw, () -> isShow = !state.rules.waves && state.isCampaign());
+        Events.run(EventType.Trigger.draw, () -> button.visible = !state.rules.waves && state.isCampaign());
     }
 
     @Override
     public Table setTable() {
         return new Table(t -> {
             t.name = name;
-            t.button(icon, Styles.clearTogglei, this::onClick).size(50f).visible(isShow);
+            t.add(button).size(50f);
         });
     }
 
