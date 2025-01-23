d099e4a0bc81b3cf71444d4ac7d168f2401ed33c
Ancvike <2047870696@qq.com>
2025-01-23 14:40:25 +0800

    测试

--- src/auxiliary/ui/SettingUI.java
+++ src/auxiliary/ui/SettingUI.java
@@ -17,7 +17,7 @@ public class SettingUI {
     public static final Seq<MyKeyBind> keys = new Seq<>();
 
     public static void init() {
-        keys.addAll(RECOVERY_BUDDING, RECOVERY_UNIT, TEST);
+        keys.addAll(RECOVERY_BUDDING, RECOVERY_UNIT);
         Vars.ui.settings.addCategory("AuxiliaryMod设置", t -> {
             if (Core.app.isDesktop()) {
                 for (MyKeyBind key : keys) {
