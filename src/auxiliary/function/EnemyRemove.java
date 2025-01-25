cf708de812630bdf2403d780a550030732eb0eea
Ancvike <2047870696@qq.com>
2025-01-25 23:22:07 +0800

    测试

--- src/auxiliary/function/EnemyRemove.java
+++ src/auxiliary/function/EnemyRemove.java
@@ -4,8 +4,6 @@ import mindustry.game.Team;
 import mindustry.gen.Building;
 import mindustry.gen.Icon;
 
-import static mindustry.Vars.state;
-
 public class EnemyRemove extends Function {
     public EnemyRemove() {
         super("enemy-remove", Icon.eraser, "清除废墟");
@@ -13,10 +11,8 @@ public class EnemyRemove extends Function {
 
     @Override
     public void onClick() {
-        if (!state.rules.waves && state.isCampaign()) {
-            for (Building building : Team.derelict.data().buildings) {
-                building.kill();
-            }
+        for (Building building : Team.derelict.data().buildings) {
+            building.kill();
         }
     }
 }
