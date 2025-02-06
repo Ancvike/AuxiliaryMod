2312e38e1c160977e529ba119b4a261ad8664683
Ancvike <2047870696@qq.com>
2025-02-06 15:43:56 +0800

    测试

--- src/auxiliary/binding/KeyBind_Keyboard.java
+++ src/auxiliary/binding/KeyBind_Keyboard.java
@@ -47,7 +47,7 @@ public class KeyBind_Keyboard extends Table {
             }
         });
 
-        Events.run(EventType.Trigger.update, () -> {
+        Events.run(EventType.Trigger.draw, () -> {
             if (Core.input.keyDown(MyKeyBind.RECOVERY_BUDDING.nowKeyCode) && isTap) {
                 endX = World.toTile(Core.input.mouseWorld().x);
                 endY = World.toTile(Core.input.mouseWorld().y);
@@ -68,14 +68,14 @@ public class KeyBind_Keyboard extends Table {
                 }
             }
         });
-        Events.run(EventType.Trigger.update, () -> {
+        Events.run(EventType.Trigger.draw, () -> {
             if (Core.input.keyTap(MyKeyBind.RECOVERY_BUDDING.nowKeyCode)) {
                 startX = World.toTile(Core.input.mouseWorld().x);
                 startY = World.toTile(Core.input.mouseWorld().y);
                 isTap = true;
             }
         });
-        Events.run(EventType.Trigger.update, () -> {
+        Events.run(EventType.Trigger.draw, () -> {
             if (Core.input.keyRelease(MyKeyBind.RECOVERY_BUDDING.nowKeyCode) && state.isPlaying()) {
                 if (!state.rules.waves && state.isCampaign()) {
                     for (Building building : player.team().data().buildings) {
