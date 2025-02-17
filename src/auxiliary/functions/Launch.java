e4bd11fdb8366059fd35fce41a15496989d156d4
Ancvike <2047870696@qq.com>
2025-02-17 18:42:31 +0800

    测试

--- src/auxiliary/functions/Launch.java
+++ src/auxiliary/functions/Launch.java
@@ -1,8 +1,21 @@
 package auxiliary.functions;
 
-public class Launch extends Function{
+import mindustry.Vars;
+
+import static mindustry.Vars.state;
+
+public class Launch extends Function {
 
     public Launch() {
         super(0, "从此区块发射");
+    }
+
+    @Override
+    public void onClick() {
+        if (state.isCampaign()) {
+            Vars.ui.hudfrag.showToast("111");
+        }else {
+            Vars.ui.hudfrag.showToast("当前功能仅在战役中使用");
+        }
     }
 }
