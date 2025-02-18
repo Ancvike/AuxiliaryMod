91a7bfbd38b5e75367d3e32443a935bc68717de7
Ancvike <2047870696@qq.com>
2025-02-18 15:51:40 +0800

    测试

--- src/auxiliary/functions/Launch.java
+++ src/auxiliary/functions/Launch.java
@@ -1,11 +1,12 @@
 package auxiliary.functions;
 
 import mindustry.Vars;
+import mindustry.ui.dialogs.PlanetDialog;
 
 import static mindustry.Vars.state;
 
 public class Launch extends Function {
-
+    public PlanetDialog dialog;
     public Launch() {
         super(0, "从此区块发射");
     }
@@ -13,7 +14,8 @@ public class Launch extends Function {
     @Override
     public void onClick() {
         if (state.isCampaign()) {
-            Vars.ui.planet.show();
+            dialog = Vars.ui.planet;
+            dialog.show();
         } else {
             Vars.ui.hudfrag.showToast("当前功能仅在战役中使用");
         }
