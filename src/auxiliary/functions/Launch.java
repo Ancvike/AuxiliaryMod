0d79aa54e248a45b992f1269e140195dd9f6363d
Ancvike <2047870696@qq.com>
2025-02-18 16:48:42 +0800

    测试

--- src/auxiliary/functions/Launch.java
+++ src/auxiliary/functions/Launch.java
@@ -1,16 +1,9 @@
 package auxiliary.functions;
 
-import arc.func.Cons;
 import mindustry.Vars;
-import mindustry.type.Sector;
-import mindustry.ui.dialogs.PlanetDialog;
-
-import static mindustry.Vars.control;
 import static mindustry.Vars.state;
-import static mindustry.ui.dialogs.PlanetDialog.Mode.look;
 
 public class Launch extends Function {
-    public MyPlanetDialog dialog;
 
     public Launch() {
         super(0, "从此区块发射");
@@ -19,21 +12,9 @@ public class Launch extends Function {
     @Override
     public void onClick() {
         if (state.isCampaign()) {
-            dialog = (MyPlanetDialog) Vars.ui.planet;
-            dialog.showSelect(state.rules.sector, other -> {
-                if (other.planet == state.rules.sector.planet) {
-                    control.playSector(other);
-                }
-            });
+
         } else {
             Vars.ui.hudfrag.showToast("当前功能仅在战役中使用");
         }
-    }
-}
-
-class MyPlanetDialog extends PlanetDialog {
-    @Override
-    public void showSelect(Sector sector, Cons<Sector> listener) {
-
     }
 }
