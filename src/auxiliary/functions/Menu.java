b616cd16e8662c3ba05608cdbe0a88f7994065aa
Ancvike <2047870696@qq.com>
2025-02-28 23:06:38 +0800

    测试

--- src/auxiliary/functions/Menu.java
+++ src/auxiliary/functions/Menu.java
@@ -16,6 +16,7 @@ import arc.util.Tmp;
 import mindustry.Vars;
 import mindustry.game.EventType;
 import mindustry.gen.Icon;
+import mindustry.gen.Tex;
 import mindustry.graphics.Pal;
 import mindustry.ui.Styles;
 import mindustry.ui.dialogs.BaseDialog;
@@ -38,28 +39,15 @@ public class Menu {
         Vars.ui.hudGroup.fill(t -> {
             t.name = "auxiliary-functions";
             t.add(button).row();
-            if (mobile) t.button(Icon.android, Styles.selecti, () -> isOpen = !isOpen);
+            if (mobile) {
+                ImageButton.ImageButtonStyle style = new ImageButton.ImageButtonStyle();
+                style.imageUp = Tex.button;
+                style.imageOver = Tex.buttonOver;
+                t.button(Icon.android, style, () -> isOpen = !isOpen);
+            }
             t.right();
         });
         button.addListener(new DragListener(button));
-
-        Vars.ui.hudGroup.fill(t -> {
-            t.button(Icon.android, Styles.defaulti, () -> {});
-            t.button(Icon.android, Styles.nodei, () -> {});
-            t.button(Icon.android, Styles.emptyi, () -> {});
-            t.button(Icon.android, Styles.emptyTogglei, () -> {});
-            t.button(Icon.android, Styles.selecti, () -> {});
-            t.button(Icon.android, Styles.logici, () -> {});
-            t.button(Icon.android, Styles.geni, () -> {});
-            t.button(Icon.android, Styles.grayi, () -> {});
-            t.button(Icon.android, Styles.flati, () -> {});
-            t.button(Icon.android, Styles.squarei, () -> {});
-            t.button(Icon.android, Styles.squareTogglei, () -> {});
-            t.button(Icon.android, Styles.clearNonei, () -> {});
-            t.button(Icon.android, Styles.cleari, () -> {});
-            t.button(Icon.android, Styles.clearTogglei, () -> {});
-            t.button(Icon.android, Styles.clearNoneTogglei, () -> {});
-        });
     }
 
     public void setDialog(BaseDialog dialog) {
