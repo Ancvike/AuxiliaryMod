1064576a117da83872eb58d18e3ca0f7b46620a7
Ancvike <2047870696@qq.com>
2025-04-04 21:06:20 +0800

    测试

--- src/auxiliary/functions/SpeedChange.java
+++ src/auxiliary/functions/SpeedChange.java
@@ -16,7 +16,7 @@ public class SpeedChange extends Function {
         float[] speeds = {1f, 2f, 5f, 10f, 20f, 50f, 100f};
         return new Table(t -> {
             t.add(speedLabel).margin(0f).pad(0f).growX();
-            t.slider(0, speeds.length - 1, 1, 1, value -> {
+            t.slider(0, speeds.length - 1, 1, 0, value -> {
                 Time.setDeltaProvider(() -> Math.min(Core.graphics.getDeltaTime() * 60.0f * value, 3.0f));
                 speedLabel = value + "x";
             }).margin(0f).pad(0f).growX();
