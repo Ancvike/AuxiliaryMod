d1b07417ec9944c8e1c3c500a37db6c44ba92a21
Ancvike <2047870696@qq.com>
2025-04-05 21:35:44 +0800

    测试

--- src/auxiliary/functions/TeamChange.java
+++ src/auxiliary/functions/TeamChange.java
@@ -2,11 +2,13 @@ package auxiliary.functions;
 
 import arc.scene.ui.ImageButton;
 import arc.scene.ui.layout.Table;
+import mindustry.game.Gamemode;
 import mindustry.game.Team;
 import mindustry.gen.Tex;
 import mindustry.ui.Styles;
 
 import static mindustry.Vars.player;
+import static mindustry.Vars.state;
 
 public class TeamChange extends Function {
     public TeamChange() {
@@ -22,8 +24,15 @@ public class TeamChange extends Function {
                 button.margin(4f);
                 button.getImageCell().grow();
                 button.getStyle().imageUpColor = team.color;
-                button.clicked(() -> player.team(team));
+                button.clicked(() -> {
+                    if (state.rules.mode() == Gamemode.sandbox) {
+                        player.team(team);
+                    } else {
+                        player.sendMessage("[scarlet]只能在沙盒模式中改变队伍");
+                    }
+                });
                 button.update(() -> button.setChecked(player.team() == team));
+
                 t.add(button).size(40f);
 
                 if (i++ % 3 == 2) t.row();
