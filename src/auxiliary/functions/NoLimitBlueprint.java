3c83f8398255e402c94d636615df475d574a60d9
Ancvike <2047870696@qq.com>
2025-05-05 14:18:33 +0800

    测试

---
+++ src/auxiliary/functions/NoLimitBlueprint.java
@@ -0,0 +1,27 @@
+package auxiliary.functions;
+
+import arc.scene.ui.CheckBox;
+import arc.scene.ui.layout.Table;
+import mindustry.Vars;
+
+public class NoLimitBlueprint extends Function {
+    boolean isOpen = false;
+
+    public NoLimitBlueprint() {
+        super(0, new Table(t -> t.add("蓝图大小无限制")));
+    }
+
+    public Table function() {
+        return new Table(t -> {
+            CheckBox box = new CheckBox("");
+            box.update(() -> box.setChecked(isOpen));
+            box.changed(() -> {
+                isOpen = !isOpen;
+                if (isOpen) Vars.maxSchematicSize = 1000;
+                else Vars.maxSchematicSize = 64;
+            });
+
+            t.add(box);
+        });
+    }
+}
