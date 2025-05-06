f216c5e3a74fdb104a01d3ad7448731641c470c6
Ancvike <2047870696@qq.com>
2025-05-06 17:03:01 +0800

    测试

--- src/auxiliary/functions/mapEditor/MyMapEditorDialog.java
+++ src/auxiliary/functions/mapEditor/MyMapEditorDialog.java
@@ -1,38 +1,8 @@
 package auxiliary.functions.mapEditor;
 
-import arc.files.Fi;
-import arc.util.Log;
 import mindustry.editor.MapEditorDialog;
-import mindustry.game.Rules;
-import mindustry.io.MapIO;
 
-import static mindustry.Vars.*;
-import static mindustry.Vars.state;
 
 public class MyMapEditorDialog extends MapEditorDialog {
-    boolean shownWithMap = false;
 
-    public MyMapEditorDialog() {
-        super();
-        if (!shownWithMap) {
-            logic.reset();
-            state.rules = new Rules();
-            editor.beginEdit(200, 200);
-        }
-        shownWithMap = false;
-    }
-
-    @Override
-    public void beginEditMap(Fi file) {
-        ui.loadAnd(() -> {
-            try {
-                shownWithMap = true;
-                editor.beginEdit(MapIO.createMap(file, true));
-                show();
-            } catch (Exception e) {
-                Log.err(e);
-                ui.showException("@editor.errorload", e);
-            }
-        });
-    }
 }
