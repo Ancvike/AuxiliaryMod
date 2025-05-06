e5ee88c5da6fe0af0ddbfe33033f7c36b95136e5
Ancvike <2047870696@qq.com>
2025-05-06 14:42:44 +0800

    测试

--- src/auxiliary/AuxiliaryMod.java
+++ src/auxiliary/AuxiliaryMod.java
@@ -1,9 +1,14 @@
 package auxiliary;
 
+import arc.Events;
 import auxiliary.binding.KeyBind;
 import auxiliary.functions.Menu;
+import auxiliary.functions.mapEditor.MyMapEditorDialog;
 import auxiliary.ui.SettingUI;
+import mindustry.game.EventType;
 import mindustry.mod.Mod;
+
+import static mindustry.Vars.ui;
 
 public class AuxiliaryMod extends Mod {
     @Override
@@ -11,5 +16,7 @@ public class AuxiliaryMod extends Mod {
         new Menu();
         new KeyBind().init();
         new SettingUI().init();
+
+        Events.on(EventType.ClientLoadEvent.class, e -> ui.editor = new MyMapEditorDialog());
     }
 }
