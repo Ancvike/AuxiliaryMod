5d13f3630860f52ccf3597fb2353a3732564d402
Ancvike <2047870696@qq.com>
2025-05-07 16:02:15 +0800

    测试

--- src/auxiliary/functions/mapEditor/MyMapEditorDialog.java
+++ src/auxiliary/functions/mapEditor/MyMapEditorDialog.java
@@ -1,11 +1,30 @@
 package auxiliary.functions.mapEditor;
 
+import arc.files.Fi;
+import arc.util.Log;
 import mindustry.editor.MapEditorDialog;
+import mindustry.io.MapIO;
+
+import static mindustry.Vars.editor;
+import static mindustry.Vars.ui;
 
 public class MyMapEditorDialog extends MapEditorDialog {
 
-    // 必须显式调用父类构造函数
     public MyMapEditorDialog() {
-        super(); // 关键：调用父类无参构造函数
+        super();
+    }
+
+    @Override
+    public void beginEditMap(Fi file) {
+        ui.loadAnd(() -> {
+            try {
+                Log.info("Loading map...");
+//                editor.beginEdit(MapIO.createMap(file, true));
+//                show();
+            } catch (Exception e) {
+                Log.err(e);
+                ui.showException("@editor.errorload", e);
+            }
+        });
     }
 }
