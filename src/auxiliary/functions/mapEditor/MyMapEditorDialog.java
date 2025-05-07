1c8ee9a84347be65b4658bdef6549b43ff5f1da6
Ancvike <2047870696@qq.com>
2025-05-07 17:31:27 +0800

    测试

--- src/auxiliary/functions/mapEditor/MyMapEditorDialog.java
+++ src/auxiliary/functions/mapEditor/MyMapEditorDialog.java
@@ -1,12 +1,7 @@
 package auxiliary.functions.mapEditor;
 
-import arc.files.Fi;
 import arc.util.Log;
 import mindustry.editor.MapEditorDialog;
-import mindustry.io.MapIO;
-
-import static mindustry.Vars.editor;
-import static mindustry.Vars.ui;
 
 public class MyMapEditorDialog extends MapEditorDialog {
 
@@ -14,18 +9,7 @@ public class MyMapEditorDialog extends MapEditorDialog {
         super();
     }
 
-    @Override
-    public void beginEditMap(Fi file) {
-        ui.loadAnd(() -> {
-            try {
-                Log.info("Loading map...");
-                editor.beginEdit(MapIO.createMap(file, true));
-                show();
-                editor.renderer.updateAll();
-            } catch (Exception e) {
-                Log.err(e);
-                ui.showException("@editor.errorload", e);
-            }
-        });
+    public void resumeEditing(){
+        Log.info("Resuming editing...");
     }
 }
