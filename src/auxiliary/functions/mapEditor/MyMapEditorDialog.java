71ff301fd04518e8dfe8848560e86bab3ee9a412
Ancvike <2047870696@qq.com>
2025-05-07 12:56:11 +0800

    测试

--- src/auxiliary/functions/mapEditor/MyMapEditorDialog.java
+++ src/auxiliary/functions/mapEditor/MyMapEditorDialog.java
@@ -4,4 +4,8 @@ import mindustry.editor.MapEditorDialog;
 
 public class MyMapEditorDialog extends MapEditorDialog {
 
+    // 必须显式调用父类构造函数
+    public MyMapEditorDialog() {
+        super(); // 关键：调用父类无参构造函数
+    }
 }
