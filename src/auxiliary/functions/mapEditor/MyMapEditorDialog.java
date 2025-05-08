acec9c32f895288090f170621af8da6d0c9b9da5
Ancvike <2047870696@qq.com>
2025-05-08 16:23:29 +0800

    测试

--- src/auxiliary/functions/mapEditor/MyMapEditorDialog.java
+++ src/auxiliary/functions/mapEditor/MyMapEditorDialog.java
@@ -1,73 +1,180 @@
 package auxiliary.functions.mapEditor;
 
-import arc.files.Fi;
+import arc.Core;
+import arc.scene.ui.ScrollPane;
 import arc.util.Log;
+import arc.util.Structs;
+import arc.util.Time;
 import mindustry.editor.*;
-import mindustry.io.MapIO;
+import mindustry.game.Gamemode;
+import mindustry.game.Rules;
+import mindustry.gen.Icon;
 import mindustry.maps.Map;
-import mindustry.world.Tile;
-import mindustry.world.WorldContext;
+import mindustry.ui.Styles;
+import mindustry.ui.dialogs.BaseDialog;
+import mindustry.ui.dialogs.MapPlayDialog;
 
 import static mindustry.Vars.*;
+import static mindustry.Vars.platform;
 
 public class MyMapEditorDialog extends MapEditorDialog {
-    private final Context context = new Context();
+    private MapView view;
+    private MapInfoDialog infoDialog;
+    private MapResizeDialog resizeDialog;
+    private MapGenerateDialog generateDialog;
+    private SectorGenerateDialog sectorGenDialog;
+    private BaseDialog menu;
+    private boolean shownWithMap = false;
 
     public MyMapEditorDialog() {
-        super();
-    }
+        background(Styles.black);
 
-    @Override
-    public void beginEditMap(Fi file) {
-        ui.loadAnd(() -> {
-            try {
-                Map map = MapIO.createMap(file, true);
-                editor.tags.putAll(map.tags);
-                if (map.file.parent().parent().name().equals("1127400") && steam) {
-                    editor.tags.put("steamid", map.file.parent().name());
+        view = new MapView();
+        infoDialog = new MapInfoDialog();
+        generateDialog = new MapGenerateDialog(true);
+        sectorGenDialog = new SectorGenerateDialog();
+
+        menu = new BaseDialog("@menu");
+        menu.addCloseButton();
+
+        float swidth = 180f;
+
+        menu.cont.table(t -> {
+            t.defaults().size(swidth, 60f).padBottom(5).padRight(5).padLeft(5);
+
+            t.button("@editor.savemap", Icon.save, this::save);
+
+            t.button("@editor.mapinfo", Icon.pencil, () -> {
+                infoDialog.show();
+                menu.hide();
+            });
+
+            t.row();
+
+            t.button("@editor.generate", Icon.terrain, () -> {
+                generateDialog.show(generateDialog::applyToEditor);
+                menu.hide();
+            });
+
+            t.button("@editor.resize", Icon.resize, () -> {
+                resizeDialog.show();
+                menu.hide();
+            });
+
+            t.row();
+
+            t.button("@editor.import", Icon.download, () -> {
+            });
+
+            t.button("@editor.export", Icon.upload, () -> {
+            });
+
+            t.row();
+
+            t.button("@editor.ingame", Icon.right, () -> Log.info("ingame"));
+
+            t.button("@editor.playtest", Icon.play, () -> Log.info("playtest"));
+        });
+
+        menu.cont.row();
+
+        if (steam) {
+            menu.cont.button("@editor.publish.workshop", Icon.link, () -> {
+                Map builtin = maps.all().find(m -> m.name().equals(editor.tags.get("name", "").trim()));
+
+                if (editor.tags.containsKey("steamid") && builtin != null && !builtin.custom) {
+                    platform.viewListingID(editor.tags.get("steamid"));
+                    return;
                 }
-                editor.load(() -> MapIO.loadMap(map, context));
-                renderer.resize(editor.width(), editor.height());
-                //editor.beginEdit(MapIO.createMap(file, true));
-                show();
-            } catch (Exception e) {
-                Log.err(e);
-                ui.showException("@editor.errorload", e);
+
+                Map map = save();
+
+                if (editor.tags.containsKey("steamid") && map != null) {
+                    platform.viewListing(map);
+                    return;
+                }
+
+                if (map == null) return;
+
+                if (map.tags.get("description", "").length() < 4) {
+                    ui.showErrorMessage("@editor.nodescription");
+                    return;
+                }
+
+                if (!Structs.contains(Gamemode.all, g -> g.valid(map))) {
+                    ui.showErrorMessage("@map.nospawn");
+                    return;
+                }
+
+                platform.publish(map);
+            }).padTop(-3).size(swidth * 2f + 10, 60f).update(b -> b.setText(editor.tags.containsKey("steamid") ? editor.tags.get("author", "").equals(steamPlayerName) ? "@workshop.listing" : "@view.workshop" : "@editor.publish.workshop"));
+
+            menu.cont.row();
+        }
+
+        menu.cont.button("@editor.sectorgenerate", Icon.terrain, () -> {
+            menu.hide();
+            sectorGenDialog.show();
+        }).padTop(!steam ? -3 : 1).size(swidth * 2f + 10, 60f);
+        menu.cont.row();
+
+        menu.cont.row();
+
+        menu.cont.button("@quit", Icon.exit, () -> {
+            Log.info("tryExit");
+            //tryExit();
+            menu.hide();
+        }).padTop(1).size(swidth * 2f + 10, 60f);
+
+        resizeDialog = new MapResizeDialog((width, height, shiftX, shiftY) -> {
+            if (!(editor.width() == width && editor.height() == height && shiftX == 0 && shiftY == 0)) {
+                ui.loadAnd(() -> editor.resize(width, height, shiftX, shiftY));
             }
         });
-    }
-}
 
-class Context implements WorldContext {
-    @Override
-    public Tile tile(int index) {
-        return world.tiles.geti(index);
-    }
+        new MapLoadDialog(map -> ui.loadAnd(() -> {
+            try {
+                editor.beginEdit(map);
+            } catch (Exception e) {
+                ui.showException("@editor.errorload", e);
+                Log.err(e);
+            }
+        }));
 
-    @Override
-    public void resize(int width, int height) {
-        world.resize(width, height);
-    }
+        setFillParent(true);
 
-    @Override
-    public Tile create(int x, int y, int floorID, int overlayID, int wallID) {
-        Tile tile = new EditorTile(x, y, floorID, overlayID, wallID);
-        editor.tiles().set(x, y, tile);
-        return tile;
-    }
+        clearChildren();
+        margin(0);
 
-    @Override
-    public boolean isGenerating() {
-        return world.isGenerating();
-    }
+        update(() -> {
+            if (hasKeyboard()) {
+                Log.info("doInput");
+                //doInput();
+            }
+        });
 
-    @Override
-    public void begin() {
-        world.beginMapLoad();
-    }
+        shown(() -> {
 
-    @Override
-    public void end() {
-        world.endMapLoad();
+            if (!Core.settings.getBool("landscape")) platform.beginForceLandscape();
+            editor.clearOp();
+            Core.scene.setScrollFocus(view);
+            if (!shownWithMap) {
+                //clear units, rules and other unnecessary stuff
+                logic.reset();
+                state.rules = new Rules();
+                editor.beginEdit(200, 200);
+            }
+            shownWithMap = false;
+
+            Time.runTask(10f, platform::updateRPC);
+        });
+
+        hidden(() -> {
+            editor.clearOp();
+            platform.updateRPC();
+            if (!Core.settings.getBool("landscape")) platform.endForceLandscape();
+        });
+
+        shown(this::build);
     }
 }
