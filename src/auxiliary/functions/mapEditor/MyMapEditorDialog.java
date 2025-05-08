dd5f5f19e7b0e12b0a60634c8e50eeb5229ecbda
Ancvike <2047870696@qq.com>
2025-05-08 15:42:12 +0800

    测试

--- src/auxiliary/functions/mapEditor/MyMapEditorDialog.java
+++ src/auxiliary/functions/mapEditor/MyMapEditorDialog.java
@@ -4,11 +4,14 @@ import arc.files.Fi;
 import arc.util.Log;
 import mindustry.editor.*;
 import mindustry.io.MapIO;
+import mindustry.maps.Map;
+import mindustry.world.Tile;
+import mindustry.world.WorldContext;
 
-import static mindustry.Vars.editor;
-import static mindustry.Vars.ui;
+import static mindustry.Vars.*;
 
 public class MyMapEditorDialog extends MapEditorDialog {
+    private final Context context = new Context();
 
     public MyMapEditorDialog() {
         super();
@@ -18,7 +21,14 @@ public class MyMapEditorDialog extends MapEditorDialog {
     public void beginEditMap(Fi file) {
         ui.loadAnd(() -> {
             try {
-                editor.beginEdit(MapIO.createMap(file, false));
+                Map map = MapIO.createMap(file, true);
+                editor.tags.putAll(map.tags);
+                if (map.file.parent().parent().name().equals("1127400") && steam) {
+                    editor.tags.put("steamid", map.file.parent().name());
+                }
+                editor.load(() -> MapIO.loadMap(map, context));
+                renderer.resize(editor.width(), editor.height());
+                //editor.beginEdit(MapIO.createMap(file, true));
                 show();
             } catch (Exception e) {
                 Log.err(e);
@@ -26,5 +36,38 @@ public class MyMapEditorDialog extends MapEditorDialog {
             }
         });
     }
+}
 
+class Context implements WorldContext {
+    @Override
+    public Tile tile(int index) {
+        return world.tiles.geti(index);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        world.resize(width, height);
+    }
+
+    @Override
+    public Tile create(int x, int y, int floorID, int overlayID, int wallID) {
+        Tile tile = new EditorTile(x, y, floorID, overlayID, wallID);
+        editor.tiles().set(x, y, tile);
+        return tile;
+    }
+
+    @Override
+    public boolean isGenerating() {
+        return world.isGenerating();
+    }
+
+    @Override
+    public void begin() {
+        world.beginMapLoad();
+    }
+
+    @Override
+    public void end() {
+        world.endMapLoad();
+    }
 }
