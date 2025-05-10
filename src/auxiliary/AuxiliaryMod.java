4713533aa6631dee34d37bc102a92e0d5f46965e
Ancvike <2047870696@qq.com>
2025-05-10 11:25:00 +0800

    测试

--- src/auxiliary/AuxiliaryMod.java
+++ src/auxiliary/AuxiliaryMod.java
@@ -2,12 +2,8 @@ package auxiliary;
 
 import auxiliary.binding.KeyBind;
 import auxiliary.functions.Menu;
-import auxiliary.functions.mapEditor.MyMapEditorDialog;
 import auxiliary.ui.SettingUI;
 import mindustry.mod.Mod;
-
-import static mindustry.Vars.ui;
-
 
 public class AuxiliaryMod extends Mod {
     @Override
@@ -15,7 +11,5 @@ public class AuxiliaryMod extends Mod {
         new Menu();
         new KeyBind().init();
         new SettingUI().init();
-
-        ui.editor = new MyMapEditorDialog();
     }
 }
