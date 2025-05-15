23512bb358ae9169a545bf92d90141732ef9edcc
Ancvike <2047870696@qq.com>
2025-05-15 22:05:52 +0800

    测试

--- src/auxiliary/binding/ChangeHP_KeyBind.java
+++ src/auxiliary/binding/ChangeHP_KeyBind.java
@@ -3,13 +3,19 @@ package auxiliary.binding;
 import arc.Core;
 import arc.Events;
 import arc.graphics.Color;
+import arc.scene.event.Touchable;
+import arc.scene.ui.Label;
+import arc.scene.ui.Slider;
+import arc.scene.ui.layout.Table;
 import arc.struct.Seq;
-import auxiliary.functions.window.MapEditorWindow;
+import auxiliary.functions.dragFunction.DragListener;
 import mindustry.Vars;
 import mindustry.game.EventType;
 import mindustry.game.Gamemode;
 import mindustry.gen.Building;
 import mindustry.gen.Icon;
+import mindustry.gen.Tex;
+import mindustry.ui.Styles;
 
 import static mindustry.Vars.mobile;
 import static mindustry.Vars.player;
@@ -17,12 +23,13 @@ import static mindustry.Vars.player;
 public class ChangeHP_KeyBind extends KeyBind {
     public static Seq<Building> buildings;
 
-    MapEditorWindow changeHP = new MapEditorWindow(Icon.map);
+    Table changeHP = new Table();
+    boolean shown = false;
     boolean inZoom = false;
 
     public ChangeHP_KeyBind() {
-        changeHP.build();
-        Vars.ui.hudGroup.fill(t -> t.add(changeHP));
+        build();
+        Vars.ui.hudGroup.fill(t -> t.add(changeHP).right());
         if (mobile) {
             setupMobileEvents();
         } else {
@@ -72,7 +79,8 @@ public class ChangeHP_KeyBind extends KeyBind {
                     }
                 }
                 changeHP.parent.setLayoutEnabled(false);
-                changeHP.toggle();
+                shown = true;
+                changeHP.toFront();
                 changeHP.setLayoutEnabled(true);
             } else {
                 buildings = null;
@@ -81,5 +89,46 @@ public class ChangeHP_KeyBind extends KeyBind {
             Vars.ui.hudfrag.showToast(Icon.cancel, "区块未占领,无法使用该功能");
         }
         resetSelection();
+    }
+
+    public void build() {
+        changeHP.table(t -> {
+            t.table(Tex.buttonEdge1, b -> {
+                b.left();
+                b.add("血量修改").padLeft(20);
+            }).grow();
+
+            t.table(Tex.buttonEdge3, b -> b.button(Icon.cancel, Styles.emptyi, () -> {
+                shown = false;
+                inZoom = false;
+                buildings = null;
+            }).grow()).maxWidth(8 * 15f).growY();
+
+            t.touchable = Touchable.enabled;
+            t.addListener(new DragListener(changeHP));
+        }).height(8 * 6f).growX().prefWidth();
+
+        changeHP.row();
+        changeHP.table(Styles.black5, table -> {
+            table.top().left();
+
+            table.table(rules -> {
+                rules.top().left();
+
+                Label label = rules.add("100%").get();
+                Slider slider = new Slider(0, 10, 1, false);
+                slider.setValue(10f);
+                slider.changed(() -> label.setText((int) (slider.getValue() * 10) + "%"));
+                slider.change();
+                slider.moved(hp -> {
+                    for (Building building : buildings) {
+                        building.health = building.maxHealth * (int) hp * 0.1f;
+                    }
+                });
+                rules.add(slider);
+            }).grow();
+        }).grow();
+
+        changeHP.visible(() -> shown);
     }
 }
