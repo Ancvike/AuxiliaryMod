d8a8a45485cd0aff3ab6acde53fcb44b651c9b48
Ancvike <2047870696@qq.com>
2025-05-27 17:04:32 +0800

    测试

--- src/auxiliary/block/MyBlocks.java
+++ src/auxiliary/block/MyBlocks.java
@@ -1,19 +1,16 @@
 package auxiliary.block;
 
-import mindustry.content.Items;
 import mindustry.type.Category;
 import mindustry.world.Block;
 import mindustry.world.blocks.storage.Unloader;
-import mindustry.world.meta.BlockGroup;
-
-import static mindustry.type.ItemStack.with;
 
 public class MyBlocks {
 
-    public Block omnipotent_unloader = new Unloader("omnipotent_unloader") {{
-        requirements(Category.effect, with(Items.graphite, 20, Items.silicon, 20, Items.tungsten, 10));
-        speed = 60f / 11f;
-        group = BlockGroup.transportation;
-        category = Category.effect;
-    }};
+    public Block omnipotent_unloader = new Unloader("omnipotent_unloader");
+
+    public void load() {
+        omnipotent_unloader.size = 2;
+        omnipotent_unloader.health = 100;
+        omnipotent_unloader.category = Category.effect;
+    }
 }
