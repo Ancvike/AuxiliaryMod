c5c3bcbb3d474808683b29e65032b74659262929
Ancvike <2047870696@qq.com>
2025-05-27 16:30:38 +0800

    测试

--- src/auxiliary/block/MyBlocks.java
+++ src/auxiliary/block/MyBlocks.java
@@ -1,24 +1,19 @@
 package auxiliary.block;
 
 import mindustry.content.Items;
-import mindustry.content.TechTree;
 import mindustry.type.Category;
 import mindustry.world.Block;
 import mindustry.world.blocks.storage.Unloader;
 import mindustry.world.meta.BlockGroup;
 
-import static mindustry.content.Planets.erekir;
 import static mindustry.type.ItemStack.with;
 
 public class MyBlocks {
 
-    Block omnipotent_unloader = new Unloader("omnipotent_unloader") {{
+    public Block omnipotent_unloader = new Unloader("omnipotent_unloader") {{
         requirements(Category.effect, with(Items.graphite, 20, Items.silicon, 20, Items.tungsten, 10));
         speed = 60f / 11f;
         group = BlockGroup.transportation;
-    }};
 
-    public void load() {
-        erekir.techTree.children.add(new TechTree.TechNode(erekir.techTree, omnipotent_unloader, with(Items.silicon, 10)));
-    }
+    }};
 }
