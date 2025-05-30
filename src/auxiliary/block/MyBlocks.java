e4feea598b92e253fdf123677623132a725fdec5
Ancvike <2047870696@qq.com>
2025-05-30 19:48:49 +0800

    测试

--- src/auxiliary/block/MyBlocks.java
+++ src/auxiliary/block/MyBlocks.java
@@ -1,6 +1,5 @@
 package auxiliary.block;
 
-import arc.Core;
 import mindustry.content.Items;
 import mindustry.type.Category;
 import mindustry.world.Block;
@@ -13,7 +12,7 @@ public class MyBlocks {
     public Block omnipotent_unloader;
 
     public void load() {
-        omnipotent_unloader = new Unloader("114514") {{
+        omnipotent_unloader = new Unloader("omnipotent-unloader") {{
             requirements(Category.distribution, with(Items.graphite, 20, Items.silicon, 20, Items.tungsten, 10));
             alwaysUnlocked = true;
             size = 1;
