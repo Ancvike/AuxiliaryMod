2d9891fe4428127550b3b73487fc8faf7668bd45
Ancvike <2047870696@qq.com>
2025-06-01 11:38:29 +0800

    测试

--- src/auxiliary/AuxiliaryMod.java
+++ src/auxiliary/AuxiliaryMod.java
@@ -1,18 +1,21 @@
 package auxiliary;
 
 import auxiliary.binding.HPChange_KeyBind;
+import auxiliary.binding.HPChange_Mobile_KeyBind;
 import auxiliary.block.MyBlocks;
 import auxiliary.functions.Menu;
 import auxiliary.ui.SettingUI;
 import mindustry.mod.Mod;
 
+import static mindustry.Vars.mobile;
+
 public class AuxiliaryMod extends Mod {
     @Override
     public void init() {
         new Menu();
-        new HPChange_KeyBind();
+        if (mobile) new HPChange_Mobile_KeyBind();
+        else new HPChange_KeyBind();
         new SettingUI().init();
-
     }
 
     @Override
