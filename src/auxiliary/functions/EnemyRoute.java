c162652e0a5f569b97e9c6df38eef668b603b6ab
Administrator <admin@qq.com>
2025-08-06 14:32:09 +0800

    测试

---
+++ src/auxiliary/functions/EnemyRoute.java
@@ -0,0 +1,9 @@
+package auxiliary.functions;
+
+import arc.scene.ui.layout.Table;
+
+public class EnemyRoute extends Function{
+    public EnemyRoute() {
+        super(0, new Table(table -> table.add("敌人进攻路线")));
+    }
+}
