9408cf16492bb8e0853e3f29ee85f13e46716114
Administrator <admin@qq.com>
2025-08-07 17:40:27 +0800

    测试

--- src/auxiliary/functions/EnemyRoute.java
+++ src/auxiliary/functions/EnemyRoute.java
@@ -1,9 +1,80 @@
 package auxiliary.functions;
 
+import arc.Events;
+import arc.graphics.Color;
+import arc.graphics.g2d.Draw;
+import arc.graphics.g2d.Lines;
+import arc.scene.ui.CheckBox;
 import arc.scene.ui.layout.Table;
+import arc.struct.Seq;
+import mindustry.ai.Pathfinder;
+import mindustry.ai.types.SuicideAI;
+import mindustry.game.EventType;
+import mindustry.game.Team;
+import mindustry.gen.Unit;
+import mindustry.world.Tile;
 
-public class EnemyRoute extends Function{
+import static mindustry.Vars.pathfinder;
+import static mindustry.Vars.state;
+
+public class EnemyRoute extends Function {
+    boolean isOpen = false;
+
     public EnemyRoute() {
         super(0, new Table(table -> table.add("敌人进攻路线")));
+
+        Events.run(EventType.Trigger.draw, () -> {
+            if (isOpen) {
+                draw();
+                Draw.reset();
+            }
+        });
+    }
+
+    @Override
+    public Table function() {
+        return new Table(t -> {
+            CheckBox box = new CheckBox("");
+            box.update(() -> box.setChecked(isOpen));
+            box.changed(() -> isOpen = !isOpen);
+            t.add(box);
+        });
+    }
+
+    public void draw() {
+        for (Unit unit : state.rules.waveTeam.data().units) {
+            if (!unit.type.flying) {
+                Lines.stroke(1, Color.acid);
+                Seq<Tile> pathTiles = generatePathTiles(unit.tileOn(), unit.team, unit.controller() instanceof SuicideAI ? 0 : unit.pathType());
+                for (int i = 0; i < pathTiles.size - 1; i++) {
+                    Tile from = pathTiles.get(i), to = pathTiles.get(i + 1);
+                    if (from == null || to == null) continue;
+                    Lines.line(from.worldx(), from.worldy(), to.worldx(), to.worldy());
+                }
+            } else {
+                Lines.stroke(1, Color.red);
+                Seq<Tile> pathTiles = generatePathTiles(unit.tileOn(), unit.team, unit.controller() instanceof SuicideAI ? 0 : unit.pathType());
+                for (int i = 0; i < pathTiles.size - 1; i++) {
+                    Tile from = pathTiles.get(i), to = pathTiles.get(i + 1);
+                    if (from == null || to == null) continue;
+                    Lines.line(from.worldx(), from.worldy(), to.worldx(), to.worldy());
+                }
+            }
+        }
+    }
+
+    public static Seq<Tile> generatePathTiles(Tile startTile, Team team, int type) {
+        Seq<Tile> pathTiles = new Seq<>();
+
+        getNextTile(startTile, pathfinder.getField(team, type, Pathfinder.fieldCore), pathTiles);
+
+        return pathTiles;
+    }
+
+    static void getNextTile(Tile tile, Pathfinder.Flowfield field, Seq<Tile> pathTiles) {
+        Tile nextTile = pathfinder.getTargetTile(tile, field);
+        pathTiles.add(nextTile);
+        if (nextTile == tile || nextTile == null) return;
+        getNextTile(nextTile, field, pathTiles);
     }
 }
