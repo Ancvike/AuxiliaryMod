5775e18be4612e1069cb2cf127129b5fa2b0cfd1
Administrator <admin@qq.com>
2025-08-18 14:36:49 +0800

    测试

--- src/auxiliary/functions/functions/Invincibility.java
+++ src/auxiliary/functions/functions/Invincibility.java
@@ -12,50 +12,21 @@ import mindustry.gen.Unit;
 import static mindustry.Vars.player;
 
 public class Invincibility extends Function {
-    public static boolean isInvincible = false;
+    static boolean isInvincible = false;
 
-    public static Seq<Unit> invincibleUnits = new Seq<>();
-    public static Unit unitPlayer = null;
+    static Seq<Unit> invincibleUnits = new Seq<>();
+    static Unit unitPlayer = null;
 
     public Invincibility() {
         super(0, new Table(table -> table.add("当前单位和所控制单位无敌")));
-//
-//        Events.run(EventType.Trigger.update, () -> {
-//            if (isInvincible) {
-//                if (invincibleUnits.size != 0 && unitPlayer != null) {
-//                    unitPlayer.health = unitPlayer.maxHealth;
-//
-//                    for (Unit unit : invincibleUnits) {
-//                        unit.health = unit.maxHealth;
-//                    }
-//                }
-//
-//                unitPlayer = Vars.player.unit();
-//                for (Unit unit : Vars.control.input.selectedUnits) {
-//                    invincibleUnits.add(unit);
-//                }
-//
-//                player.unit().health = 99999999;
-//                for (Unit unit : Vars.control.input.selectedUnits) {
-//                    unit.health = 99999999;
-//                }
-//            }
-//        });
-//
-//        Events.on(EventType.WorldLoadEvent.class, e -> {
-//            isInvincible = false;
-//
-////            if (invincibleUnits.size != 0 && unitPlayer != null) {
-//                unitPlayer.health = unitPlayer.maxHealth;
-//
-//                for (Unit unit : invincibleUnits) {
-//                    unit.health = unit.maxHealth;
-//                }
-////            }
-//
-//            unitPlayer = null;
-//            invincibleUnits.clear();
-//        });
+        Events.run(EventType.Trigger.update, () -> {
+            if (isInvincible) {
+                player.unit().health = player.unit().maxHealth;
+                for (Unit unit : Vars.control.input.selectedUnits) {
+                    unit.health = unit.maxHealth;
+                }
+            }
+        });
     }
 
     @Override
