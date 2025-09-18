6574e15493aa393d9b58a24fcb8a1d3e23178a4d
Ancvike <2047870696@qq.com>
2025-09-18 21:37:34 +0800

    测试

--- src/auxiliary/functions/functions/MoreInformation.java
+++ src/auxiliary/functions/functions/MoreInformation.java
@@ -1,45 +1,79 @@
 package auxiliary.functions.functions;
 
+import arc.Core;
 import arc.Events;
 import arc.graphics.Color;
 import arc.graphics.g2d.Draw;
 import arc.graphics.g2d.Fill;
+import arc.graphics.gl.FrameBuffer;
+import arc.math.Angles;
 import arc.scene.ui.CheckBox;
+import arc.scene.ui.layout.Scl;
 import arc.scene.ui.layout.Table;
+import arc.util.Align;
+import arc.util.Tmp;
 import auxiliary.functions.Function;
 import mindustry.Vars;
 import mindustry.game.EventType;
+import mindustry.game.Team;
 import mindustry.gen.Building;
 import mindustry.gen.Groups;
+import mindustry.gen.Icon;
 import mindustry.gen.Unit;
 import mindustry.graphics.Pal;
+import mindustry.ui.Fonts;
 import mindustry.world.Tile;
 import mindustry.world.blocks.ConstructBlock;
+import mindustry.world.blocks.defense.turrets.ItemTurret;
+import mindustry.world.blocks.defense.turrets.LiquidTurret;
+import mindustry.world.blocks.defense.turrets.PowerTurret;
 import mindustry.world.blocks.defense.turrets.Turret;
 import mindustry.world.blocks.units.Reconstructor;
 import mindustry.world.blocks.units.UnitFactory;
 
+import static arc.Core.graphics;
+import static mindustry.Vars.renderer;
+
 public class MoreInformation extends Function {
     boolean isOpen = false;
+
+    static final float[] zIndexTeamCache = new float[Team.baseTeams.length];
+    static final FrameBuffer effectBuffer = new FrameBuffer();
 
     public MoreInformation() {
         super(0, new Table(table -> table.add("更多的信息显示").tooltip("单位血量, 单位护盾值, 建筑血量, 炮塔弹药, 工厂生产进度")));
 
+        for (int i = 0; i < Team.baseTeams.length; i++) {
+            zIndexTeamCache[i] = 166 + (Team.baseTeams.length - Team.baseTeams[i].id) * 3 * 0.001f;
+        }
+
         Events.run(EventType.Trigger.draw, () -> {
-            if (isOpen) {
-                for (Tile tile : Vars.world.tiles) {
-                    onTile(tile);
-                    Draw.reset();
-                }
+            if (!isOpen) return;
+            Core.camera.bounds(Tmp.r1);
+
+            effectBuffer.resize(graphics.getWidth(), graphics.getHeight());
+            for (float zIndex : zIndexTeamCache) {
+                Draw.drawRange(zIndex, () -> effectBuffer.begin(Color.clear), effectBuffer::end);
             }
+
+            for (Tile tile : Vars.world.tiles) {
+                onTile(tile);
+                Draw.reset();
+            }
+
+            Draw.reset();
         });
+
         Events.run(EventType.Trigger.draw, () -> {
-            if (isOpen) {
-                for (Unit unit : Groups.unit) {
-                    onUnit(unit);
-                    Draw.reset();
-                }
+            if (!isOpen) return;
+            Core.camera.bounds(Tmp.r1);
+
+            for (Unit unit : Groups.unit) {
+                onUnit(unit);
+                Draw.reset();
             }
+
+            Draw.reset();
         });
     }
 
@@ -54,14 +88,20 @@ public class MoreInformation extends Function {
     }
 
     public void onTile(Tile tile) {
-        if (tile.build == null) return;
+        if (!isInCamera(tile.worldx(), tile.worldy(), 8) || tile.build == null) return;
 
         Building b = tile.build;
 
         drawBar(b, -(b.block.size * 4 - 2), b.healthf(), Pal.health);
 
         if (b instanceof Turret.TurretBuild turretBuild) {
-            drawBar(b, b.block.size * 4 - 2, turretBuild.reloadCounter / ((Turret) b.block).reload, Pal.ammo);
+            if (b.block instanceof ItemTurret) {
+                drawBar(b, b.block.size * 4 - 2, (float) turretBuild.totalAmmo / ((ItemTurret) b.block).maxAmmo, Pal.ammo);
+
+            }
+            if (b.block instanceof LiquidTurret)
+                drawBar(b, b.block.size * 4 - 2, turretBuild.liquids.currentAmount() / b.block.liquidCapacity, Pal.lancerLaser);
+            if (b.block instanceof PowerTurret) drawBar(b, b.block.size * 4 - 2, b.power.status, Pal.ammo);
         }
         if (b instanceof ConstructBlock.ConstructBuild constructBuild)
             drawBar(b, b.block.size * 4 - 2, constructBuild.progress(), b.team.color);
@@ -73,7 +113,14 @@ public class MoreInformation extends Function {
     }
 
     public void onUnit(Unit unit) {
-        drawBar(unit, -(unit.type.hitSize - 5f), unit.healthf(), Pal.health);
+        if (isInCamera(unit.x, unit.y, unit.hitSize) && !renderer.pixelator.enabled() && unit.item() != null && unit.itemTime > 0.01f) {
+            drawBar(unit, -(unit.type.hitSize * 0.9f), unit.healthf(), Pal.health);
+
+            Fonts.outline.draw(String.valueOf(unit.stack.amount),
+                    unit.x + Angles.trnsx(unit.rotation + 180f, unit.type.itemOffsetY),
+                    unit.y + Angles.trnsy(unit.rotation + 180f, unit.type.itemOffsetY) - 3,
+                    Pal.accent, 0.25f * unit.itemTime / Scl.scl(1f), false, Align.center);
+        }
     }
 
     void drawBar(Building b, float offsetY, float progress, Color color) {
@@ -112,5 +159,10 @@ public class MoreInformation extends Function {
                 bx - width / 2, by - height / 2,
                 bx - width / 2 + width * progress, by - height / 2,
                 bx - width / 2 + width * progress, by + height / 2);
+    }
+
+    public static boolean isInCamera(float x, float y, float size) {
+        Tmp.r2.setCentered(x, y, size);
+        return Tmp.r1.overlaps(Tmp.r2);
     }
 }
