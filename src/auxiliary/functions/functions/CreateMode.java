890f10d25ac48e3d9451b57d5909fc7e16452c3a
Ancvike <2047870696@qq.com>
2025-09-19 20:44:10 +0800

    测试

--- src/auxiliary/functions/functions/CreateMode.java
+++ src/auxiliary/functions/functions/CreateMode.java
@@ -3,7 +3,6 @@ package auxiliary.functions.functions;
 import arc.Core;
 import arc.scene.ui.*;
 import arc.scene.ui.layout.Table;
-import arc.util.Scaling;
 import auxiliary.functions.Function;
 import mindustry.gen.Icon;
 import mindustry.gen.Tex;
@@ -15,8 +14,15 @@ public class CreateMode extends Function {
     public CreateMode() {
         super(0, new Table(table -> table.add("自由改造").tooltip("你可以自由地在地图上修改一切")));
 
+        createModeTable.setWidth(300f);
+        createModeTable.setHeight(300f);
         createModeTable.table(t -> {
+            t.table(Tex.buttonEdge1, b -> {
+                b.left();
+                b.add("自由改造");
+            }).grow();
 
+            t.table(Tex.buttonEdge3, b -> b.button(Icon.cancel, Styles.emptyi, () -> createModeTable.visible = false).grow()).maxWidth(8 * 15f).growY();
         });
     }
 
